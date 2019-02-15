de4b251b4b9a6cf7caa2f982c117decfb6212981
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 00:16:04 -0500

    comments

--- transcription.py
+++ transcription.py
@@ -1,10 +1,18 @@
 class Sequence:
     TranscriptionTable = {"A":"U","T":"A","C":"G","G":"C"}
+    # Dictionary with the name of the restriction enzyme and
+    # the recognition sequence.
+    EnzDict = {"EcoRI":"GAATTC","EcoRV":"GATATC"}
     def __init__(self, seqstring):
-        self.seqstring=seqstring.upper()
+        self.seqstring = seqstring.upper()
+    def restriction(self,enz):
+        try:
+            return self.seqstring.count(Sequence.EnzDict[enz])
+        except:
+            return 0
     def transcription(self):
         tt = ""
         for x in self.seqstring:
             if x in ’ATCG’:
-                tt += self.TranscriptionTable[x]
+                tt += Sequence.TranscriptionTable[x]
         return tt
