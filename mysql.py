81a4b089d333e8a37f8e824d6cf421ae26328c92
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 00:51:26 -0500

    initial commit

---
+++ mysql.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Draw markers in chromosome data from MySQL database
+
+# standard library
+import os
+import sys
+import re
+
+# local stuff
+import MySQLdb
+from Bio.Graphics import BasicChromosome
+
+# reportlab
+
+
