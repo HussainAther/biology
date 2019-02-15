58c020dce5de9e19548b6b7a1a53f31aa17fccd8
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 01:00:30 -0500

    getchromo

--- mysql.py
+++ mysql.py
@@ -40,3 +40,43 @@ def sortmarkers(crms,end):
                     pass
         i += 1
     return crms_o
+
+def getchromo(crms_o,end):
+    """
+    From an ordered list of markers,
+    generate chromosomes.
+    """
+    chromo = [[] for r in range(len(end))]
+    i = 0
+    for crm_o in crms_o:
+        j = 0
+        if len(crm_o) > 1:
+            for mark in crm_o:
+                if mark==crm_o[0]: #first marker
+                    chromo[i].append(("", None, mark[1]))
+                    chromo[i].append((mark[0],colors.red,
+                                    mark[2]-mark[1]))
+                    ant = mark[2]
+                elif mark==crm_o[-1]: #last marker
+                    chromo[i].append(("", None, mark[1]-ant))
+                    chromo[i].append((mark[0],colors.red,
+                                    mark[2]-mark[1]))
+                    chromo[i].append(("", None, end[i]-mark[2]))
+                else:
+                    chromo[i].append(("", None, mark[1]-ant))
+                    chromo[i].append((mark[0],colors.red,
+                                    ant=mark[2]))
+        elif len(crm_o)==1: # For chromosomes with one marker
+            chromo[i].append(("", None, crm_o[0][1]))
+            chromo[i].append((crm_o[0][0],colors.red,
+                  crm_o[0][2]-crm_o[0][1]))
+            chromo[i].append(("", None, end[i]-crm_o[0][2]))
+        else:
+            # For chromosomes without markers
+            # Add 3% of each chromosome
+            chromo[i].append(("", None, int(0.03*end[i])))
+            chromo[i].append(("", None, end[i]))
+            chromo[i].append(("", none, int(0.03*end[i])))
+        i += 1
+        j += 1
+    return chromo
