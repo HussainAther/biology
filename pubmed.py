204f408b675bf3b8842938ee1f6c2a30e7311344
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 00:35:46 -0500

    initial commit

---
+++ pubmed.py
@@ -0,0 +1,17 @@
+from Bio import Entrez
+
+# retrieve and display data from PubMed
+
+my_em = ’user@example.com’
+db = "gene"
+term = ’cobalamin synthase homo sapiens’
+h_search = Entrez.esearch(db=db, email=my_em, term=term)
+record = Entrez.read(h_search)
+res_ids = record["IdList"]
+for r_id in res_ids:
+    h_summ = Entrez.esummary(db=db, id=r_id, email=my_em)
+    summ = Entrez.read(h_summ)
+    print(r_id)
+    print(summ[0][’Description’])
+    print(summ[0][’Summary’])
+    print(’==============================================’)
