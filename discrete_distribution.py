a05663296638bd2fb3567ee5f296c4219863721c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 01:20:01 -0500

    initial commit

---
+++ discrete_distribution.py
@@ -0,0 +1,8 @@
+import discrete_distribution
+
+# draw numbers from discrete probability distribution
+
+discrete_distribution.seed()
+
+print sum([discrete_distribution.draw([0.2,0.2,0.3,0.3])<=
+    for x in range(10000)])/10000.
