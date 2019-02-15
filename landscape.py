f517ec13c01e10d80c57829b64c3078c4091fb58
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 01:16:12 -0500

    initial commit

---
+++ landscape.py
@@ -0,0 +1,18 @@
+import fileinput # (1)
+import numpy
+from matplotlib import pylab
+
+def plot(filename):
+    """Read single-column numbers in filename and plot zero-mean
+    cumulative sum"""
+    numbers = []
+    for line in fileinput.input(filename): # (2)
+        numbers.append(float(line.split(’\n’)[0]))
+    mean = numpy.mean(numbers) # (3)
+    cumulative_sum = numpy.cumsum([number -
+                                   mean for number in numbers])
+    pylab.plot(cumulative_sum[0::10],’k-’) # (4)
+    pylab.xlabel(’i’)
+    pylab.title(’Zero Mean Cumulative Sum’)
+    pylab.savefig(filename+’.png’) # (5)
+    pylab.show()
