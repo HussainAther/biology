da3c5412f95e0abcc9a80890e9a1d30c208899ad
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 00:19:21 -0500

    reading DNA

--- transcription.py
+++ transcription.py
@@ -16,8 +16,11 @@ class Sequence:
             if x in ’ATCG’:
                 tt += Sequence.TranscriptionTable[x]
         return tt
-        
+
 class Plasmid(Sequence):
+    """
+    Simple plasmid dictionary
+    """
     AbResDict = {"Tet":"ctagcat","Amp":"CACTACTG"}
     def __init__(self,seqstring):
         Sequence.__init__(self,seqstring)
@@ -26,3 +29,27 @@ class Plasmid(Sequence):
             return True
         else:
             return False
+
+class Forward:
+    """
+    Reading a DNA sequence in a forward direction
+    """
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+    def __iter__(self):
+        return self
+    def next(self):
+        if self.index == len(self.data):
+            raise StopIteration
+        answer = self.data[self.index]
+        self.index = self.index + 1
+        return answer
+
+class Reverse:
+    """
+    Reading a DNA sequence in reverse
+    """
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
