b43cf16d0868a6ff40336e169c4cc236fefb5e2f
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 01:20:41 -0500

    single line

--- discrete_distribution.py
+++ discrete_distribution.py
@@ -4,5 +4,4 @@ import discrete_distribution
 
 discrete_distribution.seed()
 
-print sum([discrete_distribution.draw([0.2,0.2,0.3,0.3])<=
-    for x in range(10000)])/10000.
+print sum([discrete_distribution.draw([0.2,0.2,0.3,0.3])<= for x in range(10000)])/10000.
