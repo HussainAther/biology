e22891c9e45de61054a7ea30704da1cab01bda36
Syed Hussain Ather <shussainather@gmail.com>
2019-02-16 18:13:51 -0500

    initial commit

---
+++ montecarlo.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+"""
+Monte Carlo method randomly tests data points to approxiamte or guess a solution, thus getting a good idea
+of what is going on by sampling only a few points rather than many. This approach is often coupled with trrgeting
+the selection of data points towards the most promising or important results. This leads directly into the Markov chain
+in which we can base a new selection on the last one. A famous example of this is the Metropolis-Hastings algorithm for
+making the next guess for an effectively random walk with probabilistic selection that echoes thermodynamic energy.
+
+Generating random numbers is key.
+"""
+
