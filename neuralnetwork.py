f320a408dc7bcc2edb56eed18057f273f2145b2f
Syed Hussain Ather <shussainather@gmail.com>
2019-02-16 17:23:48 -0500

    initial commit

---
+++ neuralnetwork.py
@@ -0,0 +1,16 @@
+
+
+"""
+Biological sequences can generate feature vectors for use in machine learning progarms. Here, we predict the secondary
+structure of a residue in the middle of a five-amino-acid sequence. Both the the first and secondary structures can be
+represented as code letters but then converted to zeros and ones. Then, we can pass them to the neural network.
+
+For a five-letetr protein sequence, the vtctor will have 20 elements (one for each amino acid) for each of the five
+sequence positions. The total length is 100.
+"""
+
+seqSecStrucData = [("ADTLL", "E"), ("DTLLI", "E"), ("TLLIL", "E"),
+                    ("LLILG", "E"), ("LILGD", "E"), ("ILGDS", "E"),
+                    ("LGDSL", "C"), ("GDSLS", "H"), ("DSLSA", "H"),
+                    ("SLSAG", "H"), ("LSAGY","H"), ("SAGYR", "C"),
+                    ("AGYRM", "C"), ("GYRMS", "C"), ("YRMSA", "C"), ("RMSAS","C")]
