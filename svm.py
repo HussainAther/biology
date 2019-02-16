5428d04d92a667fdf903b16ac995665db050608c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-16 17:41:12 -0500

    initial commit

---
+++ svm.py
@@ -0,0 +1,9 @@
+
+
+
+"""
+Supprt vector machines use feature vectors representing training data separated into different regions by
+a boundary line that can allow the feature space to follow a complex path. The SVM finds the boundary line by finding
+the linear hyperplane that best separates the data in higher numbers of dimensions. The decision hyperplane is in the
+middle of the widest margin in data classes, and this margin is itself determined by the support vectors.
+"""
