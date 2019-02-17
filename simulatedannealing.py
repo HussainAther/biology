4c9f8e43ab18e06b3d2e35f8478fd7c1da026ea7
Syed Hussain Ather <shussainather@gmail.com>
2019-02-16 22:02:23 -0500

    comment

--- simulatedannealing.py
+++ simulatedannealing.py
@@ -1,3 +1,14 @@
 
 """
+Using the Monte Carlo method simulated annealing we can use teh combinatorial and function optimizations
+under the main principles that the same kind of random selection and acceptance rules as before, but
+the degree of acceptance of non-improving states diminishes. As data sampling proceeds the acceptance criterion
+becomes stricter and the range of accepted steps effectively becomes narrower. Simulated annealing counters some of the later moves which
+would otherwise caue the state to jump out of a glboally optimum solution. Often simulated annelaing is used in situations in which
+it's not required to have the absolute best solution, but a reasonably good one. The genetics analogt is
+mutation and crossover of DNA strands when parents produce offspring. Starting from random positions the
+forces that result from particular arrangement of atoms leads to dynamic solutions involving force, momentum,
+and changes of atomic position.
 """
+
+def
