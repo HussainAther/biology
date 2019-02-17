0b3992904ddf48e4a7401d00af50653391690daf
Syed Hussain Ather <shussainather@gmail.com>
2019-02-17 00:17:47 -0500

    import

--- tkinter.py
+++ tkinter.py
@@ -1,5 +1,11 @@
-import tkinter
-
+import re
+try:
+    import tkinter
+    from tkinter import filedialog, messagebox
+except:
+    import Tkinter as tkinter
+    import tkFileDialog as filedialog
+    import tkMessageBox as messagebox
 """
 Tkinter used for a graphical user interface.
 """
@@ -7,3 +13,7 @@ Tkinter used for a graphical user interface.
 rootWindow = tkinter.Tk()
 
 label = tkinter.Label(rootWindow, text="Hello World")
+
+label.pack()
+
+rootWindow.mainloop()
