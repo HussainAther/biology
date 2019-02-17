d0c11ecef432901fe1a455fbcf94b3e69401c544
Syed Hussain Ather <shussainather@gmail.com>
2019-02-16 22:18:27 -0500

    comment application

--- particledynamics.py
+++ particledynamics.py
@@ -1,4 +1,5 @@
 
 """
-
+In integrating the various ways particles interact with one another, we can determine solutions that
+hold true for various folding of proteins and create models of other biochemical structures. 
 """
