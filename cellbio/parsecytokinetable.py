82e5771de4f73dbefacc7c643982f860a1ee2d81
Syed Hussain Ather <shussainather@gmail.com>
2019-02-19 16:55:53 -0500

    initial commit

---
+++ cellbio/parsecytokinetable.py
@@ -0,0 +1,47 @@
+import numpy
+import warnings
+import pandas as pd
+import os
+import pylab
+
+def read_table(filename, delimiter="\t"):
+    """
+    Read a tab-delimited table
+    """
+    return pd.read_csv(filename, sep="\t")
+
+def parse_warnings(table):
+    """
+    Return warnings
+    """
+    data = table[1:, 1:]
+    warning_dict = {}
+    idx = -1
+    for x in data.ravel():
+        try:
+            float(x)
+        except:
+            if x not in warning_dict:
+                warning_dict[x] = idx
+                idx -= 1
+    return warning_dict
+
+def parse_samples(table):
+    """
+    Parse the samples from the table
+    """
+    headers = table[0,:]
+    sample_mapper = {}
+    for i, sample in enumerate(samples):
+        sample_mapper.setdefault(sample, []).append(i)
+    return sample_mapper
+
+
+def parse_cytokine_table(filename):
+    # extract data and sample and cytokine mapping dictinaries from table
+    table = read_table(filename)
+    warning_dict = parse_warnings(table)
+    sample_mapper = parse_samples(table)
+    cytokine_mapper = parse_headers(table)
+    data = parse_data(table, warning_dict)
+    return warning_dict, sample_mapper, cytokine_mapper, data
