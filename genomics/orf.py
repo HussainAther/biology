b5cc066227ff5691557fea7571021fc15e2fe487
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 15:32:40 -0500

    split

--- genomics/orf.py
+++ genomics/orf.py
@@ -7,4 +7,14 @@ without stop codons. An ORF is a region of nucleotides with no in frame stop cod
 
 record = SeqIO.read("https://raw.githubusercontent.com/biopython/biopython/master/Tests/GenBank/NC_005816.fna", "fasta")
 table = 11 # It's a bacterial sequence so we use NCbi codon table 11 for the appropriate translation process.
-min_pro_len = 100 
+min_pro_len = 100
+
+# Using the split method from Seq object, we can get a list of all the possible ORF translations in the six reading frames.
+for strand, nuc in [(_1, record.seq), -1, record.seq.reverse_complement())]:
+    for frame in range(3):
+        length = 3 * ((len(record)-frame) // 3) # make sure the length is a multiple of 3
+        for pro in nuc[frame:frame+length].translate(table).split("*"):
+            if len(pro) >= min_pro_len:
+                print("%s...%s - length %i, strand %i, frame %i" \
+                    % (pro[:30], pro--3:], len(pro), strand, frame))
+
