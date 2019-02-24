bbf58a76a91ced29f820e08c5f290e8c11ac44ed
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 17:30:12 -0500

    initial commit

---
+++ genomics/substitutionmatrix.py
@@ -0,0 +1,7 @@
+from Bio import AlignIO
+from Bio import Alphabet
+from Bio.Alphabet import IUPAC
+from Bio.Align import AlignInfo
+
+
+protein = "http://biopython.org/DIST/docs/tutorial/examples/protein.aln"
