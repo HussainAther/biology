a40706a13235f65a0d17767c440c662581a0b8d4
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 15:37:52 -0500

    find and print

--- genomics/orf.py
+++ genomics/orf.py
@@ -19,3 +19,35 @@ for strand, nuc in [(_1, record.seq), -1, record.seq.reverse_complement())]:
                     % (pro[:30], pro--3:], len(pro), strand, frame))
 
 # Now we'll use the condition that we always count from the 5' end (start) of the forward strand
+record = SeqIO.read("https://raw.githubusercontent.com/biopython/biopython/master/Tests/GenBank/NC_005816.gb", "genbank")
+
+def find_orfs_with_trans(seq, trans_table, min_protein_length):
+    answer = []
+    seq_len = len(seq)
+    for strand, nuc in [(+1, seq), (-1, seq.reverse_complement())]:
+        for frame in range(3):
+            trans = str(nuc[frame:].translate(trans_table))
+            trans_len = len(trans)
+            aa_start = 0
+            aa_end = 0
+            while aa_start < trans_len:
+                aa_end = trans.find("*", aa_start)
+                if aa_end == -1:
+                    aa_end = trans_len
+                if aa_end-aa_start >= min_protein_length:
+                    if strand == 1:
+                        start = frame+aa_start*3
+                        end = min(seq_len, frame+aa_end*3+3)
+                    else:
+                        start = seq_len-frame-aa_end*3-3
+                        end = seq_len-frame-aa_start*3
+                    answer.append((start,end,strand,trans[aa_start:aa_end]))
+                aa_start = aa_end+1
+    answer.sort()
+    return answer
+
+orf_list = find_orfs_with_trans(record.seq, table, min_pro_len)
+for start, end, strand, pro in orf_list
+    prnit("%s...%s - length %i, strand %i, %i:%i" \
+        % (pro[:30], pro-3:], len(pro), strand, start, end))
+
