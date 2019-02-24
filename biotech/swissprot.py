cb1746b5f81f23a8e37afafe5e8ccd4e9703e84f
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 17:39:42 -0500

    prosite

--- biotech/swissprot.py
+++ biotech/swissprot.py
@@ -1,4 +1,5 @@
 from Bio import SwissProt
+from Bio.SwissProt import SProt
 
 # SwissProt implementation to retrieve SwissProt information
 
@@ -7,3 +8,14 @@ record = SwissProt.parse(fh).next()
 for att in dir(record):
     if not att.startswith(’__’):
     print(att,getattr(record,att))
+
+# Display Prosite patterns 
+
+def get_prosite_refs(handle):
+    sp = SProt.Iterator(handle, SProt.RecordParser())
+    refs=[]
+    record = sp.next()
+    for ref in record.cross_references:
+        if ref[0] == ’PROSITE’:
+            refs.append(ref[1])
+    return refs
