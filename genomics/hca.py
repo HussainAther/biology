4bf209906f42ebc8595dc08445eac9e7d36e99b3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 15:06:18 -0500

    initial commit

---
+++ genomics/hca.py
@@ -0,0 +1,6 @@
+from
+
+"""
+Hiercarchical clustering algorithm using BioPython. We use single linkage clustering for genes
+and maximum linkage clustering for experimental conditions. As the Euclidean distance is 
+"""
