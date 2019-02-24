3724b0f72304a30990ab53e872b3bda609fcd670
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 15:10:42 -0500

    start coding

--- genomics/hca.py
+++ genomics/hca.py
@@ -1,6 +1,11 @@
-from
+from Bio import Cluster
 
 """
 Hiercarchical clustering algorithm using BioPython. We use single linkage clustering for genes
-and maximum linkage clustering for experimental conditions. As the Euclidean distance is 
+and maximum linkage clustering for experimental conditions. As the Euclidean distance is
 """
+
+with open("https://raw.githubusercontent.com/biopython/biopython/master/Tests/Cluster/cyano.txt" as handle):
+    record = Cluster.read(handle)
+
+genetree = record.treecluster(method = "s") # s means pairwise single-linkage clustering
