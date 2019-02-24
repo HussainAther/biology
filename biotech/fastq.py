8991a449d83a5c06faaa81ff3b8383085dae3d9e
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 17:15:04 -0500

    trim adaptors

--- biotech/fastq.py
+++ biotech/fastq.py
@@ -28,3 +28,27 @@ primer_reads = (rec for rec in \
                 if rec.seq.startswith("GATGACGGTGT"))
 count = SeqIO.write(primer_reads, "with_primer.fastq", "fastq")
 print("Saved %i reads" % count)
+
+"""
+Trim adaptors
+"""
+def trim_adaptors(records, adaptor):
+    """Trims perfect adaptor sequences.
+
+    This is a generator function, the records argument should
+    be a list or iterator returning SeqRecord objects.
+    """
+    len_adaptor = len(adaptor) #cache this for later
+    for record in records:
+        index = record.seq.find(adaptor)
+        if index == -1:
+            #adaptor not found, so won't trim
+            yield record
+        else:
+            #trim off the adaptor
+            yield record[index+len_adaptor:]
+
+original_reads = SeqIO.parse(ftp, "fastq")
+trimmed_reads = trim_adaptors(original_reads, "GATGACGGTGT")
+count = SeqIO.write(trimmed_reads, "trimmed.fastq", "fastq")
+print("Saved %i reads" % count)
