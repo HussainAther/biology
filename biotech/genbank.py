9963434252e4c15c710e4cc70db8faf5404fbee3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-23 21:25:40 -0500

    intial commit

---
+++ biotech/genbank.py
@@ -0,0 +1,18 @@
+import string
+"""
+ Extracting the complete CDS from a GenBank entry
+"""
+
+def get_complete_cds(seqrecord):
+
+    if string.find(seqrecord.description, ’complete cds’) == -1:
+        return None
+    for feature in seqrecord.features:
+        if feature.key == "CDS":
+            for qualifier in feature.qualifiers:
+                if qualifier == ’location’:
+                    location = qualifier.value
+                    break
+            break
+    seq = seqrecord.seq
+    return seq[location.start-1:location.end+1]
