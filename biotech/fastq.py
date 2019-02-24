3365c24c9fd29400656eff4f5cad14649f8e85bf
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 17:14:07 -0500

    trim primers

--- biotech/fastq.py
+++ biotech/fastq.py
@@ -4,7 +4,9 @@ from Bio import SeqIO
 Simple quality control for fastq files. Works on URLs and present directories.
 """
 count = 0
-for rec in SeqIO.parse("ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR020/SRR020192/SRR020192.fastq", "fastq"):
+ftp = "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR020/SRR020192/SRR020192.fastq"
+
+for rec in SeqIO.parse(ftp, "fastq"):
     count += 1
 print("%i reads" % count)
 
@@ -12,7 +14,17 @@ print("%i reads" % count)
 Filter for read PHREd quality 20
 """
 good_reads = (rec for rec in \
-              SeqIO.parse("ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR020/SRR020192/SRR020192.fastq", "fastq") \
+              SeqIO.parse(ftp, "fastq") \
               if min(rec.letter_annotations["phred_quality"]) >= 20)
 count = SeqIO.write(good_reads, "good_quality.fastq", "fastq")
+print("Saved %i reads" % count)
+
+"""
+Trim off primers
+"""
+
+primer_reads = (rec for rec in \
+                SeqIO.parse(ftp, "fastq") \
+                if rec.seq.startswith("GATGACGGTGT"))
+count = SeqIO.write(primer_reads, "with_primer.fastq", "fastq")
 print("Saved %i reads" % count)
