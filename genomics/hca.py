345e487acbfd2350ab6249ae653079e8ad44cf72
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 15:12:39 -0500

    comments

--- genomics/hca.py
+++ genomics/hca.py
@@ -9,3 +9,8 @@ with open("https://raw.githubusercontent.com/biopython/biopython/master/Tests/Cl
     record = Cluster.read(handle)
 
 genetree = record.treecluster(method = "s") # s means pairwise single-linkage clustering
+genetree.scale()
+exptree = record.treecluster(dist="u", transpose=1)
+# u means uncentered pearson correlation (equivalent to the cosine
+# of the angle between two data vectors)
+# transpose=1 means the centroids of the columns of data
