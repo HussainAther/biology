59db2f7d7dfa889fa3e10f76d6b06398e51dff4d
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 17:33:20 -0500

    polar AA

--- genomics/substitutionmatrix.py
+++ genomics/substitutionmatrix.py
@@ -10,3 +10,12 @@ residues are to substitute for each other. It's essential for sequence compariso
 
 filename = "http://biopython.org/DIST/docs/tutorial/examples/protein.aln"
 alpha = Alphabet.Gapped(IUPAC.protein)
+c_align = alignIO.read(filename, "clustal", alphabet = alpha) # from the output of a Clustalw alignment
+summary_align = AlignInfo.SummaryInfo(c_align) # create the summary object from which we can determine substitutions
+
+
+# Look at only amino acids with polar charged side chains by ignoring non-polar amino acids
+replace_info = summary_align.replacement_dictionary(["G", "A", "V", "L", "I",
+                                                    "M", "P", "F", "W", "S",
+                                                    "T", "N", "Q", "Y", "C"])
+
