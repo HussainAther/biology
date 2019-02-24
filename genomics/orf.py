e98ef5c3dfac5fe342759c2dffb72c9f782d603a
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 15:29:13 -0500

    comment

--- genomics/orf.py
+++ genomics/orf.py
@@ -1,3 +1,4 @@
 """
-
+Identify possible genes by looking for open reading frames (ORF). We need all sixx frames for long regions
+without stop codons. An ORF is a region of nucleotides with no in frame stop codons.
 """
