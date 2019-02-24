04b1d409efb914da58ca80727c5da7b897f0aad3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 17:11:11 -0500

    phred quality

--- biotech/fastq.py
+++ biotech/fastq.py
@@ -7,3 +7,12 @@ count = 0
 for rec in SeqIO.parse("ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR020/SRR020192/SRR020192.fastq", "fastq"):
     count += 1
 print("%i reads" % count)
+
+"""
+Filter for read PHREd quality 20
+"""
+good_reads = (rec for rec in \
+              SeqIO.parse("ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR020/SRR020192/SRR020192.fastq", "fastq") \
+              if min(rec.letter_annotations["phred_quality"]) >= 20)
+count = SeqIO.write(good_reads, "good_quality.fastq", "fastq")
+print("Saved %i reads" % count)
