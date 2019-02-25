6d18cae15e62823fd29d3d522f624653b730dbf8
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 21:32:30 -0500

    comment

--- cellbio/nucleotide.py
+++ cellbio/nucleotide.py
@@ -1,4 +1,8 @@
+from Bio import SeqIO
 
+"""
+Use dot plot to visually compare nucleotide sequences for similarity to each other.
+"""
 
 
 url = "https://raw.githubusercontent.com/biopython/biopython/master/Doc/examples/ls_orchid.fasta"
