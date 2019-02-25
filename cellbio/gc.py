7f96055b06dca01e89ae14baae822877931a0562
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 21:28:56 -0500

    url and comment

--- cellbio/gc.py
+++ cellbio/gc.py
@@ -2,5 +2,8 @@ from Bio import SeqIO
 from Bio.SeqUtils import GC
 
 """
-
+Calculate GC content of ls orchid fasta.
 """
+
+url = "https://raw.githubusercontent.com/biopython/biopython/master/Doc/examples/ls_orchid.fasta"
+
