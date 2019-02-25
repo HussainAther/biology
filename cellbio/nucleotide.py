8556ac134d8ebccbdb0d9294f7a5ad442faa8170
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 21:33:29 -0500

    handle

--- cellbio/nucleotide.py
+++ cellbio/nucleotide.py
@@ -4,6 +4,12 @@ from Bio import SeqIO
 Use dot plot to visually compare nucleotide sequences for similarity to each other.
 """
 
-
 url = "https://raw.githubusercontent.com/biopython/biopython/master/Doc/examples/ls_orchid.fasta"
+
+with open(url) as in_handle:
+    record_iterator = Seq().parse(in_handle, "fasta")
+    rec_one = next(record_iterator)
+    rec_two = next(record_iterator)
+
+window = 7
 
