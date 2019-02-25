5c29e874df9ebfed4a1380a6d92f09d53c837b06
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 21:29:32 -0500

    get values

--- cellbio/gc.py
+++ cellbio/gc.py
@@ -7,3 +7,4 @@ Calculate GC content of ls orchid fasta.
 
 url = "https://raw.githubusercontent.com/biopython/biopython/master/Doc/examples/ls_orchid.fasta"
 
+gc_values = sorted(GC(rec.seq) for rec in SeqIO.parse(url, "fasta"))
