cca2935239290d96770579947b73d2c14aa5b87b
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 21:29:57 -0500

    plot

--- cellbio/gc.py
+++ cellbio/gc.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO
 from Bio.SeqUtils import GC
+import pylab
 
 """
 Calculate GC content of ls orchid fasta.
@@ -8,3 +9,6 @@ Calculate GC content of ls orchid fasta.
 url = "https://raw.githubusercontent.com/biopython/biopython/master/Doc/examples/ls_orchid.fasta"
 
 gc_values = sorted(GC(rec.seq) for rec in SeqIO.parse(url, "fasta"))
+
+pylab.plot(gc_values)
+pylab.title(
