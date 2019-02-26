9d04e00914b986975c29df61b2845d13ec8cf0ae
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 15:29:31 -0500

    init

--- ecology/populationdynamics.py
+++ ecology/populationdynamics.py
@@ -9,7 +9,26 @@ class LMatrix():
     Allow for any number of age classes or stages.
     """
     def __init__(self, stAges):
+        """
+        Initialize the variabels we'll use.
+        """
         import numpy as np
         import numpy.matlib as M
         from numpy.matlib import rand, zeros, ones, empty, eye
         import interval
+
+        self.stAges = stAges # number of age/stage classes
+        self.m = zeros((self.stAges, self.stAges)) # our matrix
+        self.step = 0 # begin at 0
+        self.popvec = None
+        self.survival = None
+        self.recurrence = None
+        self.fecundity = None
+
+    def LM_AddFecundity(self, fvector):
+        """
+        Set fecundity values for an LMatrix
+        Set first row of the matrix equal to values, and find mismatches
+        between the length of the vector and the width of the matrix.
+        Leave those positions unchanged.
+        """
