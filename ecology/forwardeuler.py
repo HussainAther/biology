97404c24fe7648140a6e13d3ac04636595229a46
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 15:54:07 -0500

    done

--- ecology/forwardeuler.py
+++ ecology/forwardeuler.py
@@ -1,3 +1,5 @@
+from numpy import linspace, zeros
+import matplotlib.pyplot as plt
 
 """
 Forward Euler scheme (Euler's method) is a population growth model that posits an animal species poulation (N)
@@ -10,4 +12,25 @@ N(t + delta_t) - N(t) = bN(t) - dN(t)
 
 in which bN(t) is the number of births and dN(t) is the number of deaths.
 
+We can also model the population using a difference equation:
+
+N(t + delta_t) = N(t) + delta_T*r*N(t)  in which r = b-d, or the growth rate.
 """
+N0 = 100 # Start with this many animals
+r   = .1 # growth rate
+dt  = .5 # step size
+N_t = 20 # number of steps
+
+t = linspace(0, (N_t+1)*dt, N_t+2)
+N = zeros(N_t+2)
+
+N[0] = N0
+for n in range(N_t+1):
+    N[n+1] = N[n] + r*dt*N[n]
+
+numerical_sol = 'bo' if N_t < 70 else 'b-'
+plt.plot(t, N, numerical_sol, t, N_0*exp(r*t), 'r-')
+plt.legend(['numerical', 'exact'], loc='upper left')
+plt.xlabel('t'); plt.ylabel('N(t)')
+filestem = 'growth1_%dsteps' % N_t
+plt.savefig('%s.png' % filestem); plt.savefig('%s.pdf' % filestem)
