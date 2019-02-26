488f0dd6a2cc39e62881322c75a9dd9eec60fabb
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 15:41:50 -0500

    done

--- ecology/populationdynamics.py
+++ ecology/populationdynamics.py
@@ -1,3 +1,7 @@
+import numpy as num
+import numpy.matlib as M
+from numpy.matlib import rand, zeros, ones, empty, eye
+import interval
 
 """
 With python, we can create a model of population dynamics using a few different approaches.
@@ -10,13 +14,8 @@ class LMatrix():
     """
     def __init__(self, stAges):
         """
-        Initialize the variabels we'll use.
+        Initialize the variables we'll use.
         """
-        import numpy as np
-        import numpy.matlib as M
-        from numpy.matlib import rand, zeros, ones, empty, eye
-        import interval
-
         self.stAges = stAges # number of age/stage classes
         self.m = zeros((self.stAges, self.stAges)) # our matrix
         self.step = 0 # begin at 0
@@ -74,3 +73,41 @@ class LMatrix():
             print("current matrix " + str(self.m))
             self.m[toState, fromState] = value
             print("with interval " + str(self.m))
+
+    def LM_SetPopulation(self, popvector):
+        """
+        Keep size of teh population in a 1xN column vector. The actual representation is a numpy array
+        that has no column vector. It's handled in the stepping method.
+        """
+        if (povector.shape[0] == self.stAges):
+            self.popvec = popvector
+        else:
+            print("Mismatch in size: %s vs. %s" % (self.stAges, popvector.shape[0]))
+
+    def LM_StepForward(self):
+        """
+        Obtain the new population vector
+        """
+        nextpopvec = num.mat(self.m) * num.mat(self.popvec).T # Convert the population array to a numpy matrix
+                                                              # and transpose it to get the column vector we need.
+                                                              # Multiply the L*matrix by the column vector with the population
+                                                              # at the next step.
+        self.lastpopvec = self.popvec
+        self.popvec = num.array(nextpopvec.T)
+        self.step += 1
+
+    def LM_TotalPopulation(self):
+        """
+        Return total population size
+        """
+        if (None != self.popvec):
+            t= num.mat(self.popvec)*(ones(Self.stAges).T
+            return t[0,0]
+        else:
+            return 0
+
+if __name__=="__main__":
+    def hermitian(A, **kwargs): # Hermitian function
+        return num.transpose(A, **kwargs).conj()
+    T = num.transpose # shortcuts for tranpose
+    H = hermitian
