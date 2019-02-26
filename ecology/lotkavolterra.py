c936cb8ea090ba26dd4cf4ade969b54877ca19d0
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 01:27:46 -0500

    function

--- ecology/lotkavolterra.py
+++ ecology/lotkavolterra.py
@@ -1,6 +1,21 @@
 from vpython.graph import *
+from numpy import zeros
 
 """
 Compute the population dynamics for a group of predators and prey following
 the Lotka-Volterra model.
 """
+
+Tmin = 0
+Tmax = 500
+y = zeros( (2), float)
+Ntimes = 1000
+y[0] = 2.0
+y[1] = 1.3
+h = (Tmax − Tmin)/Ntimes
+t = Tmin
+
+def f( t , y, F): # some function
+    F[0] = 0.2∗y[0]∗(1 − (y[0]/(20.0) )) − 0.1∗y[0]∗y[1] # RHS 1st eq
+    F[1] = − 0.1∗y[1] + 0.1∗y[0]∗y[1]; # RHS 2nd eq
+
