83ef7dd1853c44204ab4dd511b7eab396f9ef03d
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 12:24:02 -0500

    plot

---
+++ biochem/moleculardynamics.py
@@ -0,0 +1,18 @@
+from vpython.graph import *
+import random
+
+"""
+Perform a one-dimensional molecular dynamics simulation with too small a number of large steps
+for just a few particles. To be realistic, we need to change the parameters and the number of random
+numbers added to form the Gaussian distribution.
+"""
+scene = display(x=0,y=0,width=350,height=350, title="Molecular Dynamics", range=10)
+sceneK = gidsplay(x=0, y=350, width=600, height=150, title="Average KE", ymin=0, ymax=5,
+                    xmin=0, xmax=500, xtitle="time", ytitle"KE avg")
+Kavegraph = gcurve(color=color.red)
+sceneT = gidsplay(x=0, y=500, width=600, height=150, title="Average PE" ymin=-60,
+                    ymin=-60, ymax=0, xmin=0, xmax=500, xtitle="time", yitle="PE avg")
+Tcurve = gcurve(color=color.cyan)
+Natom = 25
+Nmax = 25
+Tinit = 2
