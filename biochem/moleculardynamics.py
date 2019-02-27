5d724b1d306ef05e111b6f76e0cbf47d2368f9a2
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 12:29:23 -0500

    def velpos

--- biochem/moleculardynamics.py
+++ biochem/moleculardynamics.py
@@ -23,5 +23,34 @@ t1 = 0
 x = np.zeros((Nmax), float) # x position
 y = np.zeros((Nmax), float) # y position
 vx = np.zeros((Nmax), float) # x velocity
-vy = np.zeros((Nmax), float) # y velocity 
+vy = np.zeros((Nmax), float) # y velocity
+fx = np.zeros((Nmax, 2), float) # x force
+fy = np.zeros((Nmax, 2), float) # y force
+L = int(1*Natom**.5) # side of square with atoms
+atoms = []
+
+def twelveran(): # average of twelve arndom numbers
+    s = 0
+    for i in range(1, 13):
+        s += random.random()
+    return x/12 - .5
+
+def initialposvel(): # initial positions and velocities
+    i = -1
+    for ix in range(0, L):
+        for iy in range(0, L):
+            i += 1
+            x[i] = ix
+            y[i] = iy
+            vx[i] = twelveran()
+            vy[i] = twelveran()
+            vx[i] = vx[i] * sqrt(Tinit)
+            vy[i] = vy[i] * sqrt(Tinit)
+    for j in range(0, Natom):
+        xc = 2*x[j] - 4
+        yc = 2*y[j] - 4
+        atoms.append(sphere(pos=(xc, yc), radius=.5, color=color.red))
+
+
+
 
