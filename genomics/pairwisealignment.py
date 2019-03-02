8ba08a092125e5d9ec288a37fd800451b31a9092
Syed Hussain Ather <shussainather@gmail.com>
2019-03-02 17:46:32 -0500

    history

--- genomics/pairwisealignment.py
+++ genomics/pairwisealignment.py
@@ -1,4 +1,6 @@
 """
+Among types of pairwise alignment (Comparing two sequences), we discuss local and global alignment.
+
 TTG can be aligned with TTGCTC with two matches, one mismatch, and one gap.
 T---TG
 TTGCTC
@@ -8,6 +10,18 @@ TTGCTC
 def classicalScore():
     """
     If the substitution cost is given by a function Σ × Σ → R (all real numbers) such that d(σ, σ′) is the
-    cost of changing character σ to σ′.
+    cost of changing character σ to σ′ and the gap cost is given by a function g: N → R such that g(k) is the cost
+    of insertion or deletion of k characters, then we say the score function is a classical score function.
+    The score of the function is the sum of hte costs of each event described by the alignment.
     """
 
+
+
+
+"""
+Needleman-Wunsch algorithm for global alignment. This algorithm was published by Needleman and Wunsch in 1970
+for alignment of two protein sequences and it was the first application of dynamic programming to biological
+sequence analysis. The Needleman-Wunsch algorithm finds the best-scoring global alignment between two sequences.
+
+
+"""
