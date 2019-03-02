46c53e52ba36c725a04cea53250b0703e39eb48c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-02 18:54:06 -0500

    SSAP comment

--- genomics/structuralalignment.py
+++ genomics/structuralalignment.py
@@ -1,4 +1,3 @@
-
 """
 Use information secondary and tertiary structure of of protein or RNA molecules to
 align the sequences.
@@ -6,6 +5,15 @@ align the sequences.
 
 
 """
-DALI Method: Distance matrix alignmetn uses fragments for constructing structural
-alignemnts based on contact similarity patterns between successive 
+DALI: Distance matrix alignment uses fragments for constructing structural
+alignemnts based on contact similarity patterns between successive hexapeptides in the
+query seuqences. It generates pairwiese or multiple alignments and identifies a query sequence
+structural neighbors from the protein data bank.
 """
+
+"""
+SSAP: Sequenctial structure alignemnt uses atom-to-atom vectors in structure space as comparison
+points. It's used in multiple and pairwise alignments to create CATH (Class, Archtiecture, Topology,
+Homology) hierarchical database classification of protein folds.
+"""
+
