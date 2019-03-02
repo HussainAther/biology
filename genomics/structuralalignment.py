cb3067d086097ad1270308b9ae3b17199f1e2443
Syed Hussain Ather <shussainather@gmail.com>
2019-03-02 18:51:38 -0500

    comment

---
+++ genomics/structuralalignment.py
@@ -0,0 +1,11 @@
+
+"""
+Use information secondary and tertiary structure of of protein or RNA molecules to
+align the sequences.
+"""
+
+
+"""
+DALI Method: Distance matrix alignmetn uses fragments for constructing structural
+alignemnts based on contact similarity patterns between successive 
+"""
