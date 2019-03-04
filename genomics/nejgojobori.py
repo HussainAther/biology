b478ddb161e3345b894b9f4047df03063a29753c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 13:08:58 -0500

    explain

--- genomics/nejgojobori.py
+++ genomics/nejgojobori.py
@@ -2,5 +2,7 @@
 """
 This method computes the numbers of synonymous and nonsynonymous substitutions and the numbers
 of potentially synonymous and potentially nonsynonymous sites (Nei and Gojobori 1986).
+Uses two homologous ORF sequences as input. The algorithm uses the alignment and separately
+counts the number of non-synonymous and synonymous sites and differenecse between the sequences.
 """
 
