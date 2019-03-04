900c7bd68929176b818c08dc9d5299eec3f6a418
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 12:05:41 -0500

    pc1

--- genomics/pca.py
+++ genomics/pca.py
@@ -16,3 +16,23 @@ def principalComponentAnalysis(data, n=2):
 
     indices = evals.argsort()[::-1]
     evecs = evecs[:,indices]
+
+    basis = evecs[:,:n]
+    energy = evals[:n].sum()
+
+    return basis, energy
+
+def extractPrincnipalComponent(data, precision=1e-9):
+    samples, features = data.shape
+    meanVec = data.mean(axis=0)
+    dataC = data - meanVec
+
+    pc1 = random.random(features)
+    pc0 = pc1 - 1
+
+    while abs((pc0-pc1).sum()) > precision:
+        t = np.zeros(features)
+        for datum in dataC:
+            t += np.dot(datum, pc1) * datum
+
+            
