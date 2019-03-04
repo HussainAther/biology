5d4b5849c90fd24e5ed59f56b86cffb5d1a61c23
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 13:06:28 -0500

    comment

---
+++ genomics/nejgojobori.py
@@ -0,0 +1,6 @@
+
+"""
+This method computes the numbers of synonymous and nonsynonymous substitutions and the numbers
+of potentially synonymous and potentially nonsynonymous sites (Nei and Gojobori 1986).
+"""
+
