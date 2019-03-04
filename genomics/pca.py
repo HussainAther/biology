cb92cc189daee99096592147a18a4a1f90ad4990
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 12:01:34 -0500

    initial commit

---
+++ genomics/pca.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+"""
+Extract innate trends within data. The principal components are the vectors that give the
+best separation fo data items in terms of their covariances.
+"""
