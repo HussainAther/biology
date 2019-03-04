b923151278dd186f4e6796ce71797912c490b641
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 11:31:56 -0500

    improve

--- genomics/kmeans.py
+++ genomics/kmeans.py
@@ -22,7 +22,26 @@ def kMeans(data, k, centers=None):
     while change > 1e-8:
         clusters = [[] for x in range(k)]
         for vector in data:
+            """
+            As we define the memberships within the clusters, we calculate the centers of the clusters
+            based on data vectors that belong to each. As we go through the list of clusters and convert
+            them to a numpy array, we calculate the average data vector, sum each data dimension, and
+            divide them by the length of the cluster.
+            """
             diffs = center - vector
             dists(diffs**2).sum(axis=1)
             closest = dists.argmin()
             clusters[closest].append(vector)
+        change = 0
+        for i, cluster in enumerate(clusters):
+            cluster = np.array(cluster)
+            center = cluster.sum(axis=0)/len(cluster)
+            diff = center - centers[i]
+            change += (diff**2).sum()
+            centers[i] = center
+
+        return centers, clusters
+
+"""
+
+"""
