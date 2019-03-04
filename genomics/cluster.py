064464a33203a6ac7b4a1fb102b459f67212eb1c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 11:52:21 -0500

    neighbor

--- genomics/cluster.py
+++ genomics/cluster.py
@@ -7,4 +7,20 @@ to the arrangement of data points.
 
 def euclideanDist(vectorA, vectorB):
     diff = vectorA - vectorB
-    
+    return np.sqrt(np.dot(diff,diff))
+
+def findNeighbors(data, distFunc, threshold):
+    neighborDict = {}
+    n = len(data)
+    for i in range(n):
+        neighborDict[i] = []
+
+    for i in range(0, n-1):
+        for j in range(i+1, n):
+            dist = distFunc(data[i], data[j])
+
+            if dist < threshold:
+                neighborDict[i].append(j)
+                neighborDict[j].append(i)
+
+    return neighborDict
