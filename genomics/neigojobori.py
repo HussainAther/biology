075e39e417a3853ff2fedeb29ae3737a6035dd4a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 14:48:00 -0500

    typo

--- genomics/neigojobori.py
+++ genomics/neigojobori.py
@@ -3,7 +3,7 @@ import numpy as np
 """
 This method computes the numbers of synonymous and nonsynonymous substitutions and the numbers
 of potentially synonymous and potentially nonsynonymous sites (Nei and Gojobori 1986).
-Tses two homologous ORF sequences as input. The algorithm uses the alignment and separately
+Uses two homologous ORF sequences as input. The algorithm uses the alignment and separately
 counts the number of non-synonymous and synonymous sites and differenecse between the sequences.
 """
 
