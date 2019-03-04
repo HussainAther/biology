3672d582fedb63614592660ebd3a8ead969d5b0d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 14:56:19 -0500

    colons, difference

--- genomics/neigojobori.py
+++ genomics/neigojobori.py
@@ -104,20 +104,22 @@ def neiGojobori(a, b):
                 ss += 1
             else: # they don't code for the same amino acid
                 ns += 1
-    print("Non-synonomous sbustitutions " + str(ns))
-    print("Synonomous sbustitutions " + str(ss))
+    print("Non-synonomous subs: " + str(ns))
+    print("Synonomous subs: " + str(ss))
+    print("Their difference: " + str(ns-ss))
 
     ps = (ss / (ss + ns)) # p distance normalizes them for comparison
     pn = (ns / (ss + ns))
 
-    print("SS p distance " + str(ps))
-    print("NS p distance " + str(pn))
+    print("SS p distance: " + str(ps))
+    print("NS p distance: " + str(pn))
+    print("Their difference: " + str(ps - pn))
 
     ds = -(3/4) * np.log(1- (4/3)*ps) # Jukes-Cantor correction (JC) accounts for multiple substitutions at the same site
     dn = -(3/4) * np.log(1- (4/3)*pn)
 
-    print("JC for synonymous subs" + str(ds))
-    print("JC for non-synonymous subs" + str(dn))
-
+    print("JC for synonymous subs: " + str(ds))
+    print("JC for non-synonymous subs: " + str(dn))
+    print("Their difference: "  + str(ds - dn))
 
     return
