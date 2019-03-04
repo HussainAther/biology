c2625aeb2f48528fa1150822c54c93c241775dce
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 11:26:43 -0500

    import

--- genomics/kmeans.py
+++ genomics/kmeans.py
@@ -1,5 +1,11 @@
+import numpy as np
+from random import sample
 
 """
 The k-means clustering algorithm clusters the data into k number of clusters with each cluster
-as the central average position. 
+as the central average position. We cycle through the data while making an initial guess that
+is improved through re-appraising the data and moving the centers of the clusters to the
+average of their memberships.
 """
+
+
