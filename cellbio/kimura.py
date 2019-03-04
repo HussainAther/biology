db76cf7ebc3ca0c5ccc8491cfbb86f856676287d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 14:25:11 -0500

    initial commit

---
+++ cellbio/kimura.py
@@ -0,0 +1,6 @@
+
+"""
+Motoo Kimura's model of population genetics uses more than just the alpha parameter (which indicates the
+general probability of substitution). It uses beta as the probability of inversions.
+"""
+
