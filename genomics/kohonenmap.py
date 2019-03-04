69d277b71ffa0f1817f9959c38d7839371e04c53
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 12:12:14 -0500

    comment

--- genomics/kohonenmap.py
+++ genomics/kohonenmap.py
@@ -1,5 +1,7 @@
 
 """
 Kohonen map is a self-organizing map that rearranges data into an organized form, the "map"
-of the data that can be divided into different regions to define categories.
+of the data that can be divided into different regions to define categories. It uses dimensional reduction
+to create a low-dimensionality from the large vectors of input data. We can easily visualize the major
+differences and similarities in the data without having to htink in n-dimensional space.
 """
