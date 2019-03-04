0a0288e3b8c4d2857554be37938f7b3857b23688
Syed Hussain Ather <shussainather@gmail.com>
2019-03-04 12:03:21 -0500

    evecs

--- genomics/pca.py
+++ genomics/pca.py
@@ -4,3 +4,15 @@ import numpy as np
 Extract innate trends within data. The principal components are the vectors that give the
 best separation fo data items in terms of their covariances.
 """
+
+def principalComponentAnalysis(data, n=2):
+    samples, features = data.shape
+
+    meanVec = data.mean(axis=0)
+    dataC = (data - meanVec).T
+
+    covar = np.cov(dataC)
+    evals, evecs = np.linalg.eig(covar)
+
+    indices = evals.argsort()[::-1]
+    evecs = evecs[:,indices]
