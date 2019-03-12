518ac4de6fef78f10807c7702c72803bdd92e351
Syed Hussain Ather <shussainather@gmail.com>
2019-03-12 01:36:45 -0400

    initial commit

---
+++ biophysics/nussinov.py
@@ -0,0 +1,6 @@
+
+"""
+We implement that Nussinov RNA Folding Algorithm for predicting the secondary structure of RNA.
+We get a two-dimensional secondary structure of RNA by determining how pairing between nucleotides
+in the primary sequence give rise to a two-dimensional architecture.
+"""
