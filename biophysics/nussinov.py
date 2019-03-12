f2b0d669ef17ebe64860e12bb3c6d4dd6f38124d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-12 01:38:52 -0400

    pair

--- biophysics/nussinov.py
+++ biophysics/nussinov.py
@@ -1,6 +1,16 @@
+import sys
+import numpy as np
 
 """
 We implement that Nussinov RNA Folding Algorithm for predicting the secondary structure of RNA.
 We get a two-dimensional secondary structure of RNA by determining how pairing between nucleotides
 in the primary sequence give rise to a two-dimensional architecture.
 """
+
+def pair(t):
+    """
+    Check if the pair of nucleotides bond with one another.
+    """
+    if t in [("A", "U"), ("U", "A"), ("C", "G"), ("G", "C")]:
+        return True
+    return False
