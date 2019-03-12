03ff76eac0a413a17d76f775ed3a4ab70e147b28
Syed Hussain Ather <shussainather@gmail.com>
2019-03-12 12:37:00 -0400

    bg

--- biophysics/mccaskill.py
+++ biophysics/mccaskill.py
@@ -1,4 +1,8 @@
 
 """
+In 1990 John S. McCaskill introduced an efficient dynamic programming algorithm to compute
+the partition function (Z=∑Pexp(−E(P)/RT)) over all possible nested structures P that can be
+formed by a given RNA sequence S with E(P) = energy of structure P, R = gas constant, and
+T = temperature.
 
 """
