eafb26c35ef4c0c495f0a492a1bb8297f76c20ee
Syed Hussain Ather <shussainather@gmail.com>
2019-03-12 12:40:33 -0400

    init

--- biophysics/mccaskill.py
+++ biophysics/mccaskill.py
@@ -6,6 +6,14 @@ In 1990 John S. McCaskill introduced an efficient dynamic programming algorithm 
 the partition function (Z=∑Pexp(−E(P)/RT)) over all possible nested structures P that can be
 formed by a given RNA sequence S with E(P) = energy of structure P, R = gas constant, and
 T = temperature.
+
+Here, we provide a simplified version of the approach using a Nussinov-like energy scoring scheme,
+i.e. each base pair of a structure contributes a fixed energy term Ebp independent of its context.
+Given this, we populate two dynamic programming tables Q and Qbp. Qi,j provides the partition function
+for subsequence from position i to j, while Qbpi,j holds the partition function of the subsequence
+given the constraint that position i and j form a base pair (or 0 if no base pair is possible).
+Watson-Crick as well as GU base pairs are considered complementary. The overall partition function
+is given by Z=Q1,n for a sequence of length n.
 """
 
 def mc():
@@ -22,3 +30,15 @@ def mc():
 
 
             Q[i][j] = Q[i][j-1] + sum
+
+def init(N, q = 0.):
+    """
+    Initialize the dot bracket
+    """
+    dp = np.empty((N,N))
+    dp[:] = np.NAN
+    for i in range(N):
+        dp[i][i] = q
+        if i != 0:
+            dp[i][i-1] = q
+    return dp
