f34c20108414a75d89ac1dfd78171b1b29ec5a5b
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 01:02:53 -0400

    comment

--- genomics/bio/baumwelch.py
+++ genomics/bio/baumwelch.py
@@ -5,8 +5,6 @@ Baum-Welch algorithm is an expectation maximization algorithm used with Hidden M
 It's used in locating genes for identifying coding regions in prokaryotic DNA, analyzing
 eukaroyitc seuqences up to one million base paris long, and finding copy number variations (CNVs)
 among genome structure variation in humans.
-
-theta =
 """
 
 def bw(num_states, num_obs, observ):
@@ -14,8 +12,9 @@ def bw(num_states, num_obs, observ):
     Baum-Welch algorithm follows as:
     1. (expectation) forward-backward algorithm to get forward and backward probabilities
     2. create a two-dimensional array of probabilities p with n rows and k+1 columns
-    3. (maximization) find the probabilities of being at a state with a time using the equation for theta (above)
-    4.
+    3. (maximization) find the probabilities of being at a state with a time using the
+    probability distribution for individual states.
+    4. Repeate (iterate) expectation and maximization steps until the likelihood converges given the parameters
     """
     A_mat = np.ones( (num_states, num_states) )
     A_mat = A_mat / np.sum(A_mat,1)
