6a330b9c59853438bff31f95575c02a05a9a4916
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 00:55:30 -0400

    initial commit

---
+++ genomics/bio/baumwelch.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+"""
+Baum-Welch algorithm is an expectation maximization algorithm used with Hidden Markov Models.
+It's used in locating genes for identifying coding regions in prokaryotic DNA, analyzing
+eukaroyitc seuqences up to one million base paris long, and finding copy number variations (CNVs)
+among genome structure variation in humans.
+"""
+
