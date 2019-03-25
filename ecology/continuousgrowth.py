105241028aa68075de7def2953f556ff401cc4d7
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 21:41:27 -0400

    initial commit

---
+++ ecology/continuousgrowth.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+"""
+Among continuous growth models of population, we can look at 
+single-species models that are relevant to lab studies. But in the real world
+we can reflect multiple types of phenomena and events that occur.
+
+We'll define the rate of change in this simple context
+
+dNdt = births - deaths + migration
+"""
