29038a96470c907b8923881579e8086973d47862
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 12:43:52 -0400

    randvec

--- biotech/negativeselection.py
+++ biotech/negativeselection.py
@@ -2,3 +2,9 @@
 The Negative Selection algorithm is based off the self-nonself discrimintaino behavior
 in the mammalian acquired immune system.
 """
+
+def randvec(minmax):
+    """
+    Return a random vector from minmax.
+    """
+    
