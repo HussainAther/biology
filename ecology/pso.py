159b6c5481bc4bf47994af4bf413715744fd0b73
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 17:56:19 -0400

    random

--- ecology/pso.py
+++ ecology/pso.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 We use the Particle Swarm intelligence (particle swarm, PSO) algorithm to simulate behaviors
 of swarms and optimize these behaviors iteratively. We search for a best solution by moving
@@ -10,3 +12,6 @@ def pso(a, dim):
     For an array of particles a with dimension dim, we search for a solution that maximizes  
     the fitness value for each particle.
     """
+    x = np.random.rand(len(a))*100 # x positions of each particle
+    v = np.random.rand(len(a))*100 # v velocity of each particle
+    k = 1 # iteration
