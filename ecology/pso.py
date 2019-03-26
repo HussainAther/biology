0b8d928b9a7ac8303c33a0797dfd35653692d213
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 17:50:02 -0400

    initial commit

---
+++ ecology/pso.py
@@ -0,0 +1,6 @@
+"""
+We use the Particle Swarm intelligence (particle swarm, PSO) algorithm to simulate behaviors
+of swarms and optimize these behaviors iteratively. We search for a best solution by moving
+particles with a velocity and calculating the solution every iteration until the particle swarm
+converges to the best solution.
+"""
