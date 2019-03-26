7668ebd4f0c102b7d5f50ec8e80307c2c7f0a353
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 15:48:59 -0400

    bg

--- ecology/swarm.py
+++ ecology/swarm.py
@@ -4,4 +4,8 @@ In swarm intelligence and computational intelligence and metaheuristics, the Bee
 relates the optimal colony formation for the foraging behavior of honey bees. As honey bees 
 collecte nector from vast areas around their hive, bees communicate via a waggle dance
 to inform other bees to the direction, distance, and quality rating of food sources.
+
+We use information processing to locate and explore good sites in a problem search space.
+We randomly sample the problem space to locate good sites via a local search in which
+a small number of good sites are explored more frequently than others.
 """
