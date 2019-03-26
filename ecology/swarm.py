29b5d475e50c770753318de77f9e7392e437b7f3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 15:44:52 -0400

    initial commit

---
+++ ecology/swarm.py
@@ -0,0 +1,7 @@
+
+"""
+In swarm intelligence and computational intelligence and metaheuristics, the Bees Algorithm
+relates the optimal colony formation for the foraging behavior of honey bees. As honey bees 
+collecte nector from vast areas around their hive, bees communicate via a waggle dance
+to inform other bees to the direction, distance, and quality rating of food sources.
+"""
