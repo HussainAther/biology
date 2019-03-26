78d14f842bc6bb5e56ae9a7f52daf43d9a828c33
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 12:47:35 -0400

    eucdist

--- biotech/negativeselection.py
+++ biotech/negativeselection.py
@@ -1,3 +1,5 @@
+from random import random
+
 """
 The Negative Selection algorithm is based off the self-nonself discrimintaino behavior
 in the mammalian acquired immune system.
@@ -7,4 +9,13 @@ def randvec(minmax):
     """
     Return a random vector from minmax.
     """
-    
+    result = []
+    for i in minmax:
+        result.append(minmax[i][0] + ((minmax[i][1] - minmax[i][0]) * random())) 
+    return result
+
+def eucdist(a, b):
+    """
+    Return Euclidean distance between two vectors a and b.
+    """
+
