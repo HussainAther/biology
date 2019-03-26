bc13316050067c12f86f11261f5bbd54f2d170b0
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 18:06:50 -0400

    multidim

--- ecology/pso.py
+++ ecology/pso.py
@@ -7,26 +7,34 @@ particles with a velocity and calculating the solution every iteration until the
 converges to the best solution.
 """
  
-x = [] # x-value positions
+p = [] # positions
 v = [] # velocity values
 
-def fit(i):
+def fit(i, dim):
     """
-    Return fitness value for some particle index i .
+    Return fitness value for some particle index i and dimensions dim.
     """
-    return x[i] ** 2 + 1
+    result = 0
+    for j in range(dim):
+        result += p[j][i] ** 2 + 1
+    return result
 
 def pso(a, dim):
     """
-    For an array of particles a with dimension dim, we search for a solution that maximizes  
+    For a number of particles with dimension dim, we search for a solution that maximizes  
     the fitness value for each particle.
     """
-    x = np.random.rand(len(a))*100 # x positions of each particle
-    v = np.random.rand(len(a))*100 # v velocity of each particle
-    p_best = np.zeros(len(a)) # best fitness values of each particle
+    for i in range(dim):
+        p.append(np.random.rand(a)*100) # positions of each particle
+    for i in range(dim):
+        v.append(np.random.rand(x)*100) # each velocity component for each particle
+    p_best = np.zeros(a) # best fitness values of each particle
     k = 1 # iteration
-    for i,j in enumerate(a):
+    for i in range(a):
+        for j in range(dim):
+
          if fit(i) > p_best[i]:
              p_best[i] = fit(i) # calculate the current fitness values
     g_best = p_best.index(max(p_best)) # the particle that is the most fit
-
+    for i in range(a):
+    
