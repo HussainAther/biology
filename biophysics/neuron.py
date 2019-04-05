7d20f7edff6acc8fe2588d0d35acec48787d5478
Syed Hussain Ather <shussainather@gmail.com>
2019-04-05 15:16:54 -0400

    bg

--- biophysics/neuron.py
+++ biophysics/neuron.py
@@ -1,5 +1,9 @@
 import numpy as np
 
 """
-We can calculate the firing rate of a neuron.
+We can calculate the firing rate of a neuron by taking into account the biophysical factors involved.
+We use the realisatic firing statistics of AMPA, GABA-A, and NMDA synaptic inputs for a leaky integrate-and-fire (LIF)
+neuron that has excitatory synaptic inputs and inhibitory synaptic inputs. We also take into account the voltage-dependence
+of NMDA channels and voltage-dependence of post-synaptic currents. We incorporate finite time constant of post-synaptic currents
+into the delta-functions that describe the post-synaptic currents without duration or temporal kinetics.
 """
