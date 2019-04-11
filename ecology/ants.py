70bb5eb4dbaa6ea7bc3468038ee3dd1d5202fd48
Syed Hussain Ather <shussainather@gmail.com>
2019-04-11 17:11:20 -0400

    initial commit

---
+++ ecology/ants.py
@@ -0,0 +1,14 @@
+import numpy as np
+import wx
+import random
+
+"""
+Grooming an ant colony.
+
+Create a grid with randomly placed dead ants represented by blue squares and 
+live ants as red circles. We create a 2-D grid with wrap-around borders like
+cellular automata and compute f, the fraction of dead ants perceived by a living
+ant in its neighborhood. 
+
+Requires wxPython. 
+"""
