68a55683ef5635bcc9c54a5b06f90fd59a9d9be0
Syed Hussain Ather <shussainather@gmail.com>
2019-04-15 14:23:21 -0400

    bg

--- biophysics/photon.py
+++ biophysics/photon.py
@@ -1,5 +1,13 @@
 import numpy as np
 
 """
+By characterizing photon arrival frequency as a harmonic oscillator in terms
+of raising and lowering (creatino and annihilation operators), we may use the 
+electomagnetic field to write a Hamiltonian of a harmonic oscillator with
+frequency oemga.
 
+H = hbar * omega (a†a + 1/2)  
+
+in which a† and a are creation and annihilation operators to connect states
+with different numbers of quanta.
 """
