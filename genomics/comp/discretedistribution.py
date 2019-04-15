c3c698b71353fec976c0b99be1dd1784d7dc80fd
Syed Hussain Ather <shussainather@gmail.com>
2019-04-15 12:25:50 -0400

    bg

--- genomics/comp/discretedistribution.py
+++ genomics/comp/discretedistribution.py
@@ -1,7 +1,10 @@
 import discrete_distribution
 
 """
-Draw numbers from discrete probability distribution
+Draw numbers from discrete probability distribution.
+This could include binomial, Poisson, and negative 
+binomial distributions. In biology they're used in 
+gene distributions.
 """
 
 discrete_distribution.seed()
