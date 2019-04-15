d6d008afadf9bc26849c0ab12257995bd4ae0eb7
Syed Hussain Ather <shussainather@gmail.com>
2019-04-15 12:59:52 -0400

    print paren

--- genomics/comp/pssm.py
+++ genomics/comp/pssm.py
@@ -1,5 +1,6 @@
 import Bio.Clustalw
 import Bio.Align.AlignInfo
+
 from Bio.Alphabet import IUPAC
 from sys import *
 
@@ -15,13 +16,12 @@ align = Bio.Clustalw.parse_file(argv[1], alphabet=IUPAC.protein)
 alig_len = align.get_alignment_length()
 align_info = Bio.Align.AlignInfo.SummaryInfo(align)
 ref_seq = align.get_seq_by_num(0)
-pssm = align_info.pos_specific_score_matrix(ref_seq, chars_to_ignore = [’X’])
+pssm = align_info.pos_specific_score_matrix(ref_seq, chars_to_ignore = ["X"])
 max = len(align_info.get_column(0))
 
-# ----------------------
-print "Conservation above %d: " % threshold
+print("Conservation above %d: " % threshold)
 for pos in xrange(alig_len):
     for letter in pssm[pos].keys():
         percent = (pssm[pos][letter] / max) * 100.0
         if percent > threshold:
-            print "%d %s %3.2f%s" % (pos, letter, percent, ’%’)
+            print("%d %s %3.2f%s" % (pos, letter, percent, "%"))
