6b025bdbeaff23157abd57ad54a35757fe65937e
Syed Hussain Ather <shussainather@gmail.com>
2019-04-17 14:12:25 -0400

    quotes

--- biotech/pubmed.py
+++ biotech/pubmed.py
@@ -1,10 +1,12 @@
 from Bio import Entrez
 
-# retrieve and display data from PubMed
+"""
+Retrieve and display data from PubMed.
+"""
 
-my_em = ’user@example.com’
+my_em = "user@example.com"
 db = "gene"
-term = ’cobalamin synthase homo sapiens’
+term = "cobalamin synthase homo sapiens"
 h_search = Entrez.esearch(db=db, email=my_em, term=term)
 record = Entrez.read(h_search)
 res_ids = record["IdList"]
@@ -12,6 +14,6 @@ for r_id in res_ids:
     h_summ = Entrez.esummary(db=db, id=r_id, email=my_em)
     summ = Entrez.read(h_summ)
     print(r_id)
-    print(summ[0][’Description’])
-    print(summ[0][’Summary’])
-    print(’==============================================’)
+    print(summ[0]["Description"])
+    print(summ[0]["Summary"])
+    print("==============================================")
