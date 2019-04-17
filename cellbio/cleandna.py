59fa4a0402153d3b5fd3349ab987b9540aa10e99
Syed Hussain Ather <shussainather@gmail.com>
2019-04-17 12:41:49 -0400

    quotes

--- cellbio/cleandna.py
+++ cellbio/cleandna.py
@@ -1,9 +1,11 @@
 import re
 
-# Cleans a DNA sequence
+"""
+Cleans a DNA sequence
+"""
 
-regex = re.compile(’ |\d|\n|\t’)
-seq = ’’
-for line in open(’pMOSBlue.txt’):
-    seq += regex.sub(’’,line)
+regex = re.compile(' |\d|\n|\t')
+seq = ""
+for line in open("pMOSBlue.txt"):
+    seq += regex.sub("", line)
 print(seq)
