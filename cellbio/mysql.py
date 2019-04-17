d6dd562c59b5d58c8228bf87ced38de13ff588d3
Syed Hussain Ather <shussainather@gmail.com>
2019-04-17 12:42:54 -0400

    import spacing

--- cellbio/mysql.py
+++ cellbio/mysql.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python
-
-# Draw markers in chromosome data from MySQL database
-
-# standard library
 import os
 import sys
 import re
-
-# local stuff
 import MySQLdb
-from Bio.Graphics import BasicChromosome
 
-# reportlab
+from Bio.Graphics import BasicChromosome
 from reportlab.lib import colors
+
+"""
+Draw markers in chromosome data from MySQL database.
+"""
 
 def sortmarkers(crms,end):
     """
