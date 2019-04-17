ba808ba80e6d55c8f0e13cacb7fb3d1f0e61a2ce
Syed Hussain Ather <shussainather@gmail.com>
2019-04-17 14:21:45 -0400

    spacing

--- biotech/searchfasta.py
+++ biotech/searchfasta.py
@@ -1,13 +1,17 @@
 import re
 
-# Search for a specific pattern within a FASTA file
+"""
+Search for a specific pattern within a FASTA file.
+"""
 
-pattern = "[LIVM]{2}.RL[DE].{4}RLE"
-fh = open("/home/sb/bioinfo/prot.fas")
+pattern = "[LIVM]{2}.RL[DE].{4}RLE" # pattern in re form
+fh = open("/home/sb/bioinfo/prot.fas") # input file
 fh.readline() # Discard the first line.
+
 seq = ""
 for line in fh:
     seq += line.strip()
+
 rgx = re.compile(pattern)
 result = rgx.search(seq)
 patternfound = result.group()
