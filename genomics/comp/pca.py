09363cb0ad54aece6062206667f1f12bca26a14c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-18 13:39:17 -0400

    typo

--- genomics/comp/pca.py
+++ genomics/comp/pca.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 """
-Extract innate trends within data. The principal components are the vectors that give the
-best separation fo data items in terms of their covariances.
+Extract innate trends within data. The principal components (pca) are the vectors that give the
+best separation of data items in terms of their covariances.
 """
 
 def principalComponentAnalysis(data, n=2):
