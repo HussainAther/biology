42f2ac7c406b69607ae3dfa46b26af4767a91f01
Syed Hussain Ather <shussainather@gmail.com>
2019-04-18 13:34:04 -0400

    typo

--- genomics/comp/hca.py
+++ genomics/comp/hca.py
@@ -2,7 +2,7 @@ from Bio import Cluster
 
 """
 Hiercarchical clustering algorithm using BioPython. We use single linkage clustering for genes
-and maximum linkage clustering for experimental conditions. As the Euclidean distance is
+and maximum linkage clustering for experimental conditions.
 """
 
 # Using sample data on cyanobacteria
@@ -11,8 +11,10 @@ with open("https://raw.githubusercontent.com/biopython/biopython/master/Tests/Cl
 
 genetree = record.treecluster(method = "s") # s means pairwise single-linkage clustering
 genetree.scale()
+
 exptree = record.treecluster(dist="u", transpose=1)
 # u means uncentered pearson correlation (equivalent to the cosine
 # of the angle between two data vectors)
 # transpose=1 means the centroids of the columns of data
+
 record.save("cyanoresult", gene_tree, exptree)
