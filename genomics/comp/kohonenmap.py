cdce8472da3192a40ff0f911eabed901295c4a2f
Syed Hussain Ather <shussainather@gmail.com>
2019-04-18 13:41:54 -0400

    comment and spacing

--- genomics/comp/kohonenmap.py
+++ genomics/comp/kohonenmap.py
@@ -1,21 +1,26 @@
 import numpy as np
+
 from math import exp
 
 """
 Kohonen map is a self-organizing map that rearranges data into an organized form, the "map"
 of the data that can be divided into different regions to define categories. It uses dimensional reduction
 to create a low-dimensionality from the large vectors of input data. We can easily visualize the major
-differences and similarities in the data without having to htink in n-dimensional space.
+differences and similarities in the data without having to think in n-dimensional space.
 """
 
 def selfOrganizingMap(inputs, spread, size, steps = 1000):
+    """
+    We introduce a decay function that lets us create differences and indices
+    for the rows and columns of the map that we use to create a mesh.
+    """
     nRows, nCols = size
     vecLen = len(inputs[0])
     somap = np.random.rand(nRows,nCols,nvecLen) # self-organizing map
     
     influence = np.dstack([spread]*vecLen)
     infWidth = (len(spread)-1) /// 2
-    makeMesh = np.ix_
+    makeMesh = np.ix_ # numpy's open mesh function
 
     for s in range(steps):
         decay = exp(-2s/float(steps))
