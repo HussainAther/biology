f3a38fe30ed6f7711b0f63ff68b2852af049379e
Syed Hussain Ather <shussainather@gmail.com>
2019-04-21 16:49:33 -0400

    comment

--- genomics/comp/pca.py
+++ genomics/comp/pca.py
@@ -6,23 +6,24 @@ best separation of data items in terms of their covariances.
 """
 
 def principalComponentAnalysis(data, n=2):
+    """
+    Steps of principal component analysis
+    """
     samples, features = data.shape
-
     meanVec = data.mean(axis=0)
-    dataC = (data - meanVec).T
-
-    covar = np.cov(dataC)
-    evals, evecs = np.linalg.eig(covar)
-
+    dataC = (data - meanVec).T # take the tranpose of the data with the mean vectors
+    covar = np.cov(dataC) # covariance
+    evals, evecs = np.linalg.eig(covar) # eigenvalues and eigenvectors
     indices = evals.argsort()[::-1]
     evecs = evecs[:,indices]
-
     basis = evecs[:,:n]
     energy = evals[:n].sum()
-
     return basis, energy
 
 def extractPrincnipalComponent(data, precision=1e-9):
+    """
+    Extract the principal components
+    """
     samples, features = data.shape
     meanVec = data.mean(axis=0)
     dataC = data - meanVec
@@ -48,23 +49,19 @@ their means.
 """
 
 def twoClassLda(dataA, dataB):
+    """
+    Linear discriminant analysis
+    """
     meanA = dataA.mean(axis=0)
     meanB = dataB.mean(axis=0)
-
     covA = np.cov(dataA.T)
     covB = np.cov(dataB.T)
-
     nA = len(dataA) - 1
     nB = len(dataB) - 1
-
     scatterWithin = nA * covA + nB * covB
     scatterBetween
-
     discrim = np.dot(np.linalg.inv(scatterWithin), scatterBetween)
-
     transfA = np.dot(dataA, discrim.T)
     transfB = np.dot(dataB, discrim.T)
-
     divide = np.dot(discrim, (meanA + meanB))/2
-
     return transfA, transfB, divide
