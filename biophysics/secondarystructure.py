457b9eca468faa768037190739b86366d4a31a98
Syed Hussain Ather <shussainather@gmail.com>
2019-04-23 12:41:26 -0400

    initial commit

---
+++ biophysics/secondarystructure.py
@@ -0,0 +1,7 @@
+"""
+RNA has some of the properties of both DNA and proteins. It has the same information storage capability as DNA due to its sequence of nucleotides. But its ability to form 3D structures allows it to have enzymatic properties like those of proteins. We call the linear polynucleotide chain the primary structure of RNA; the intra-molecular base-pairing is the secondary structure.
+
+Secondary structures are important for RNA functions.  For example, tRNA molecules all form similar structures (a characteristic "clover-leaf", one branch of which is the "anticodon loop", which pairs with a codon as part of the process of translating a messenger RNA into protein).  Overall, the structures are critical to their function.
+
+Of all possible 16 base-pairings, 6 of them are considered stable.  The Watson-Crick base pairs, C-G and A-U, form stable base pairs with each other through the creation of hydrogen bonds between donor and acceptor sites on the bases.  In addition, the G-U wobble pair is weaker but still stable.  Other bases also pair sometimes, especially if chemically modified.
+"""
