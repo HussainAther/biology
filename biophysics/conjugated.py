30e98de9a6d065ddcc6822053ba498570a4b07d4
Syed Hussain Ather <shussainather@gmail.com>
2019-04-25 00:22:05 -0400

    model desc

--- biophysics/conjugated.py
+++ biophysics/conjugated.py
@@ -1,5 +1,8 @@
 """
 In the simplest model for a conjugated molecule, the electrons which form the pi orbitals
 can sit on each carbon atom with some energy we set to zero. They hop from one atom to its
-neighbors. 
+neighbors. There's one relevant electron per carbon atom. If we write the Hamiltonian for the 
+electrons as a matrix, then for a ring of six carbons (benzene) we have a "hopping matrix
+element" -t. It's negative as electrons lower their energy by being shared among neighboring
+atoms through chemical bonds.  
 """
