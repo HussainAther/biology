6aa1d858283ed6ea5c1af12d412f95da60ec17a6
Syed Hussain Ather <shussainather@gmail.com>
2019-04-25 00:20:30 -0400

    initial commit

---
+++ biophysics/conjugated.py
@@ -0,0 +1,5 @@
+"""
+In the simplest model for a conjugated molecule, the electrons which form the pi orbitals
+can sit on each carbon atom with some energy we set to zero. They hop from one atom to its
+neighbors. 
+"""
