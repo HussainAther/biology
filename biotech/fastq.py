d45bac9120aba717cab5b2356d6d84ea65c01dbd
Syed Hussain Ather <shussainather@gmail.com>
2019-05-02 16:52:33 -0400

    spacing

--- biotech/fastq.py
+++ biotech/fastq.py
@@ -15,6 +15,7 @@ print("%i reads" % count)
 """
 Filter for read PHREd quality 20
 """
+
 good_reads = (rec for rec in \
               SeqIO.parse(ftp, "fastq") \
               if min(rec.letter_annotations["phred_quality"]) >= 20)
