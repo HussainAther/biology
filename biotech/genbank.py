a7a54695bfd2091e4d1d0653cef33694c2ed1bcf
Syed Hussain Ather <shussainather@gmail.com>
2019-05-02 16:51:50 -0400

    comment

--- biotech/genbank.py
+++ biotech/genbank.py
@@ -5,7 +5,9 @@ Extracting the complete CDS from a GenBank entry
 """
 
 def get_complete_cds(seqrecord):
-
+    """
+    For a seqrecord, return the annotated coding sequences. 
+    """
     if string.find(seqrecord.description, "complete cds") == -1:
         return None
     for feature in seqrecord.features:
