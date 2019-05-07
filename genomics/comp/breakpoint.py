fd5d431e3bdbdae0bde258a59dd15a484d34c8aa
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 00:54:02 -0400

    decrease

--- genomics/comp/breakpoint.py
+++ genomics/comp/breakpoint.py
@@ -5,11 +5,20 @@ We assume the ancestor doesn't have any breakpoints and note the
 breakpoints that appear as reversals occur in the sequence. 
 """
 
-def issorted(l):
+def isSorted(l):
     """
     Is the list l sorted?
     """
     return sorted(l) == list(range(min(l), max(l)+1))
+
+def hasDecrease(l):
+    """
+    Is there a decreasing strip of numbers in l?
+    """
+    for i in range(len(l) - 1):
+        if l[i+1] > l[i]:
+            return True
+    return False
 
 def brs(a):
     """
@@ -17,5 +26,5 @@ def brs(a):
     in every step until we get the numbers in order. 
     """
     for i in a:
-        while not issorted(i):
- 
+        while not isSorted(i):
+            if  
