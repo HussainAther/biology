0d4a2d3b0db555fec2032edcc7062777fd194b62
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 00:40:26 -0400

    def

--- genomics/comp/breakpoint.py
+++ genomics/comp/breakpoint.py
@@ -4,3 +4,9 @@ what the identity permutation (ancestor) from which they originated.
 We assume the ancestor doesn't have any breakpoints and note the 
 breakpoints that appear as reversals occur in the sequence. 
 """
+
+def brs(a):
+    """
+    For an array a of sequences, remove as many breakpoints as possible
+    in every step.
+    """
