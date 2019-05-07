b3f305f1db47644ed5971941cd16dece5a63b1ca
Syed Hussain Ather <shussainather@gmail.com>
2019-05-06 23:13:42 -0400

    re

--- genomics/comp/greedymotif.py
+++ genomics/comp/greedymotif.py
@@ -1,9 +1,15 @@
+import re
+
 """
 Greedy motif search scans each DNA sequence only once. Once we have scanned a 
 particular sequence i, we decide which of its l-mer has the best contribution 
-to the partial alignment score Score(s,i,DNA) for the first i sequences and 
+to the partial alignment score Score(s, i, DNA) for the first i sequences and 
 immediately claim that this l-mer is part of the alignment.
 """
 
-def gms(s, t, n, l):
-
+def gms(s):
+    """
+    Something similar to what is described above using regex.
+    Other potential answers can be explored.
+    Return repreated motifs in s.
+    """
