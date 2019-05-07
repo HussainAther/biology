5feac41c1213632f7eef216ef944b254eb6e32f4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 00:49:20 -0400

    while

--- genomics/comp/breakpoint.py
+++ genomics/comp/breakpoint.py
@@ -5,8 +5,17 @@ We assume the ancestor doesn't have any breakpoints and note the
 breakpoints that appear as reversals occur in the sequence. 
 """
 
+def issorted(l):
+    """
+    Is the list l sorted?
+    """
+    return sorted(l) == list(range(min(l), max(l)+1))
+
 def brs(a):
     """
-    For an array a of sequences, remove as many breakpoints as possible
-    in every step.
+    For an array a of sequences of numbers, remove as many breakpoints as possible
+    in every step until we get the numbers in order. 
     """
+    for i in a:
+        while not issorted(i):
+ 
