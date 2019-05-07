a146149ac0a1f097aa6876fa8e936cef81ec6a5c
Syed Hussain Ather <shussainather@gmail.com>
2019-05-06 23:31:37 -0400

    bg

--- genomics/comp/greedymotif.py
+++ genomics/comp/greedymotif.py
@@ -5,13 +5,17 @@ Greedy motif search scans each DNA sequence only once. Once we have scanned a
 particular sequence i, we decide which of its l-mer has the best contribution 
 to the partial alignment score Score(s, i, DNA) for the first i sequences and 
 immediately claim that this l-mer is part of the alignment.
+
+For whatever scoring metric we use, the underlying motif search algorithm can
+very based on different ways of searching and identifying for a given sequence
+string.
 """
 
 def gms(s):
     """
     Something similar to what is described above using regex.
     Other potential answers can be explored.
-    Return repreated motifs in s.
+    Return repeated motifs in s.
     """
     m = re.search(r'(.+)\1+', s) 
     result = [] # output result
