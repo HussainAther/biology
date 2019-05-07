3c5d89770b5e67d51951abbfc5858f0966e368b0
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 00:38:28 -0400

    initial commit

---
+++ genomics/comp/breakpoint.py
@@ -0,0 +1,6 @@
+"""
+We may use breakpoints when dealing with permutations to figure out
+what the identity permutation (ancestor) from which they originated.
+We assume the ancestor doesn't have any breakpoints and note the 
+breakpoints that appear as reversals occur in the sequence. 
+"""
