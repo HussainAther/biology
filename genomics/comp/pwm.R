ba660a87d3c6378080887ab978d743e26f455ff0
Syed Hussain Ather <shussainather@gmail.com>
2019-05-10 01:45:42 -0400

    nuc freq

--- genomics/comp/pwm.R
+++ genomics/comp/pwm.R
@@ -26,7 +26,14 @@ pwm <- function(freq, total, bg=0.25){
   log2(p/bg)
 }
 
-# Calculate out all possible PWM values
+"Calculate out all possible PWM values."
 for (i in 0:8){
   print(pwm(i,8))
 }
+
+"Define the frequencies of nucleotides."
+a <- c(0, 4, 4, 0, 3, 7, 4, 3, 5, 4, 2, 0, 0, 4)
+c <- c(3, 0, 4, 8, 0, 0, 0, 3, 0, 0, 0, 0, 2, 4)
+g <- c(2, 3, 0, 0, 0, 0, 0, 0, 1, 0, 6, 8, 5, 0)
+t <- c(3, 1, 0, 0, 5, 1, 4, 2, 2, 4, 0, 0, 1, 0)
+m <- matrix(data=c(a,c,g,t), nrow=4, byrow=T,dimnames=list(c("a", "c", "g", "t")))
