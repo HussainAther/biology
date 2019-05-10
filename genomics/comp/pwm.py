ae6985b2dec0128095c1a09b7cc38cdc0828292b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-10 01:34:01 -0400

    profile

--- genomics/comp/pwm.py
+++ genomics/comp/pwm.py
@@ -1,4 +1,5 @@
 """
-We use a probability weight matrix (PWM) to describe the frequency of each
-nucleotide at each location in a motif.
+We use a probability weight matrix (PWM also called profile matrix) to describe 
+the frequency of each nucleotide at each location in a motif. We compute the 
+frequency of each base in each position of the suspected motif. 
 """
