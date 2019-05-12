803b40be901fdc094adc5ccfce461ac34bdb9942
Syed Hussain Ather <shussainather@gmail.com>
2019-05-12 00:31:49 -0400

    bg

--- biophysics/cooperative.py
+++ biophysics/cooperative.py
@@ -1,4 +1,6 @@
 """
 We can look at how molecules work toegether with one another to cooperate 
-when forming macromolecules.
+when forming macromolecules. We can show how the rod elasticity models of polymers
+can use phenomenological parameters of the stretch (fractional change in length
+of a segment), bend (how the rod becomes deformed), and twist density (torsional deformation).
 """
