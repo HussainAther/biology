cf00a4e8b5f87e7eabf2fd8fd91316ca30db616f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-12 00:33:27 -0400

    def ee

--- biophysics/cooperative.py
+++ biophysics/cooperative.py
@@ -4,3 +4,8 @@ when forming macromolecules. We can show how the rod elasticity models of polyme
 can use phenomenological parameters of the stretch (fractional change in length
 of a segment), bend (how the rod becomes deformed), and twist density (torsional deformation).
 """
+
+def ee():
+    """
+    Elastic energy of a polymer using a one-parameter phenomenological model.
+    """
