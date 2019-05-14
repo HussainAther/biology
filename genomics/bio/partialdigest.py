0cb482b9794483d5deeab30dde4e6b65fe392a3f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 02:22:32 -0400

    def

--- genomics/bio/partialdigest.py
+++ genomics/bio/partialdigest.py
@@ -1,5 +1,5 @@
 """
-The Simplified Partial Digest Problem uses primary fragments and base fragments to locate 
+The Simplified Partial Digest Problem (simplified partial digest) uses primary fragments and base fragments to locate 
 restriction sites. Base fragments have two endpoints that were consecutive sites on the
 target DNA strand and can be obtained by exposing the strand to the enzyme until the digestion process is
 complete. We consider there are n restriction sites where the enzyme cuts along a DNA strand of length D.
@@ -8,3 +8,8 @@ fragments {Xi - Xi-1}1 ≤ i ≤ n+1 and primary fragments {(Xn+1 - Xi) ∪ Xi}1
 X1,...,Xn, where Xi corresponds to the distance between the leftmost end of the target DNA strand and the ith
 furthest restriction site along the strand. 
 """
+
+def spd(l):
+    """
+    For some length list of dna l, find the actual places of cut.
+    """
