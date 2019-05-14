390b22c9cfb8da02050e3a33a104d252466e984a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 14:16:09 -0400

    np and spacing

--- genomics/bio/localalignment.py
+++ genomics/bio/localalignment.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Local (local) alignment
 
@@ -7,24 +9,23 @@ as usual, stop when we reach an entry with a score of .0
 """
 
 def local_align(x, y, score=ScoreParam(-7, 10, -5)):
-     """
+    """
     Do a local alignment between x and y.
     """
-     # create a zero-filled matrix
-     A = make_matrix(len(x) + 1, len(y) + 1)
-     best = 0
-     optloc = (0,0)
-     # fill in A in the right order
-     for i in xrange(1, len(y)):
-         for j in xrange(1, len(x)):
-             # the local alignment recurrance rule:
-             A[i][j] = max(
-             A[i][j-1] + score.gap,
-             A[i-1][j] + score.gap,
-             A[i-1][j-1] + (score.match if x[i] == y[j] else score.mismatch), 0)
-             # track the cell with the largest score
-             if A[i][j] >= best:
-                 best = A[i][j]
-                 optloc = (i,j) 
+    A = np.zeros((len(x) + 1, len(y) + 1)) # create a zero-filled matrix
+    best = 0
+    optloc = (0,0)
+    # fill in A in
+    for i in xrange(1, len(y)):
+        for j in xrange(1, len(x)):
+            # the local alignment recurrance rule:
+            A[i][j] = max(
+            A[i][j-1] + score.gap,
+            A[i-1][j] + score.gap,
+            A[i-1][j-1] + (score.match if x[i] == y[j] else score.mismatch), 0)
+            # track the cell with the largest score
+            if A[i][j] >= best:
+                best = A[i][j]
+                optloc = (i,j) 
     # return the opt score and the best location
     return best, optloc
