d2a69fded9630e51eab68819a794777ac9d418c9
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 16:33:47 -0400

    init

--- ecology/evolution.py
+++ ecology/evolution.py
@@ -2,6 +2,13 @@ from string import letters
 from random import choice, random
 
 """
-Evolution (evolution) algorithm.
+Evolution (evolution) algorithm on a string.
 It's like Pokemon. Except not.
 """
+
+target  = list("METHINKS IT IS LIKE A WEASEL")
+charset = letters + " "
+parent  = [choice(charset) for _ in range(len(target))]
+minmutaterate  = .09
+C = range(100)
+ 
