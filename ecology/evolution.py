d61322e55b0396dbe03c0ec9bf022e376dc5be09
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 16:34:32 -0400

    fitness

--- ecology/evolution.py
+++ ecology/evolution.py
@@ -11,4 +11,11 @@ charset = letters + " "
 parent  = [choice(charset) for _ in range(len(target))]
 minmutaterate  = .09
 C = range(100)
+
+perfectfitness = float(len(target))
  
+def fitness(trial):
+    """
+    Sum of matching chars by position across a trial for our target.
+    """
+    return sum(t==h for t,h in zip(trial, target)) 
