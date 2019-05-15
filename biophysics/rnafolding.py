8d5edf844186c437bc803f0105036eaeebbdb958
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 20:25:30 -0400

    initial commit

---
+++ biophysics/rnafolding.py
@@ -0,0 +1,8 @@
+"""
+RNAs adopt complex three-dimensional structures that are important for many biological functions. 
+Pairs of positions in RNA with complementary nucleotides can form bonds. Bonds (i, j) and (i′, j′) 
+are interleaving if i < i′ < j < j′ and noninterleaving otherwise (fig. 6.30). Every set of 
+noninterleaving bonds corresponds to a potential RNA structure. In a very naive formulation of the 
+RNA folding problem, one tries to find a maximum set of noninterleaving bonds. The more adequate model, 
+attempting to find a fold with the minimum energy, is much more difficult.
+"""
