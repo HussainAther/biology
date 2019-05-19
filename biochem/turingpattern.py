90a87843adcf1db198d9299e1a34f73261c06275
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 20:53:37 -0400

    eq

--- biochem/turingpattern.py
+++ biochem/turingpattern.py
@@ -3,4 +3,8 @@ Alan Turing partial differential equation (PDE) models for reaction-diffusion sy
 with state variables u and v as chemical concentratinos, a b c and d parameters that 
 determine the behavior of the reaction terms, with h and k constants, and Du and Dk as
 diffusion constants.
+
+dudt = a(u-h) + b(v-k) + DuΔ^2u
+
+dudt = c(u-h) + d(v-k) + DuΔ^2u
 """
