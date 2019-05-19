9385f14b8b6323237f3eb48e20ff3894895e9b0f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 20:59:50 -0400

    init

--- biochem/turingpattern.py
+++ biochem/turingpattern.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib
 matplotlib.use("TkAgg")
 from pylab import *
@@ -18,3 +19,15 @@ These equations form the Turing pattern.
 n = 100 # grid size n x n
 Dh = 1/n # spatial resolution
 Dt = .02 # temporal resolution
+a, b, c, d, h, k = 1, -1, 2, -1.5, 1, 1 # parameters
+Du = .0001 # u diffusion constant
+Dv = .0006 # v diffusion constant
+
+# Initialize equation values
+u = np.zeros([n, n])
+v = np.zeros([n, n])
+for x in xrange(n):
+    for y in xrange(n):
+        u[x, y] = 1 + uniform(-.03, .03) # small noise
+        v[x, y] = 1 + uniform(-.03, .03)
+nextu = np.zeros([n, n]) 
