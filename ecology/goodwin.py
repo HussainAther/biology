04548c77e173ede7209b8700481846d5dc93cc75
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 20:43:50 -0400

    init commit'

---
+++ ecology/goodwin.py
@@ -0,0 +1,6 @@
+"""
+Goodwin proposed a simple model for regulatory mechanisms that depend upon
+feedback control. Certain metabolites may repress enzymes essential for their
+own synthesis. They inhibit transcription of the molecule DNA to messenger
+RNA that forms the enzyme template.
+"""
