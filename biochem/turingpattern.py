a2127e8ca57e5477a70a61472e90544008eab5b9
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 20:56:59 -0400

    import

--- biochem/turingpattern.py
+++ biochem/turingpattern.py
@@ -1,3 +1,7 @@
+import matplotlib
+matplotlib.use("TkAgg")
+from pylab import *
+
 """
 Alan Turing partial differential equation (PDE) models for reaction-diffusion systems 
 with state variables u and v as chemical concentratinos, a b c and d parameters that 
@@ -7,4 +11,6 @@ diffusion constants.
 dudt = a(u-h) + b(v-k) + DuΔ^2u
 
 dudt = c(u-h) + d(v-k) + DuΔ^2u
+
+These equations form the Turing pattern.
 """
