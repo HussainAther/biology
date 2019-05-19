08d0b68e3d293603fee187958aa4af5672d4be58
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 20:46:22 -0400

    def dPdt

--- ecology/goodwin.py
+++ ecology/goodwin.py
@@ -4,3 +4,10 @@ feedback control. Certain metabolites may repress enzymes essential for their
 own synthesis. They inhibit transcription of the molecule DNA to messenger
 RNA that forms the enzyme template.
 """
+
+def dPdt(dE, e, k, P):
+    """
+    According to Michaelis-Menten kinetics, degradation of product saturates  
+    for large P depends upon differential of E (enzyme concentration), P
+    product concentraiton, reaction rate k, and constant e. 
+    """
