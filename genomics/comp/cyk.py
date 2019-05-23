b381c8782e84766b2487f637e124cef42e9bf817
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 04:05:59 -0400

    import

--- genomics/comp/cyk.py
+++ genomics/comp/cyk.py
@@ -1,4 +1,20 @@
+import numpy as np
+import nltk
+
 """
 Cocke-Younger-Kasami (CYK Cocke younger kasami) algorithm to calculate
 the most optimal parse tree for a sequence.
 """
+
+tokens = ["the", "kids", "opened", "the", "box", "on", "the", "floor"]
+
+grammar = nltk.parse_cfg("""
+    S -> NP VP
+    PP -> P NP
+    NP -> Det N | NP PP
+    VP -> V NP | VP PP
+    Det -> 'the'
+    N -> 'kids' | 'box' | 'floor'
+    V -> 'opened'
+    P -> 'on'
+    """)
