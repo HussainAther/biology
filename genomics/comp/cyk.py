38f7acdb9ed69bc82df2eaae4d5c664fedbababa
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 04:12:17 -0400

    fill in table

--- genomics/comp/cyk.py
+++ genomics/comp/cyk.py
@@ -21,6 +21,9 @@ grammar = nltk.parse_cfg("""
     """)
 
 def init_wfst(tokens, grammar):
+    """
+    Initialize the WFST table.
+    """
     numtokens = len(tokens)
     # fill with dots
     wfst = [["." for i in range(numtokens+1)]
@@ -29,4 +32,24 @@ def init_wfst(tokens, grammar):
     for i in range(numtokens):
         productions = grammar.productions(rhs=tokens[i])
         wfst[i][i+1] = productions[0].lhs()
+    return wfst 
+
+def complete_wfst(wfst, tokens, trace=False):
+    """
+    Fill in the rest of the table.
+    """
+    index = {}
+    for prod in grammar.productions(): # make reverse lookup
+        index[prod.rhs()] = prod.lhs()
+    numtokens = len(tokens)
+    for span in range(2, numtokens+1):
+        for start in range(numtokens+1-span): # go down diagonal
+            end = start + span
+            for mid in range(start+1, end):
+                nt1, nt2 = wfst[start][mid], wfst[mid][end]
+                if (nt1,nt2) in index:
+                    if trace:
+                        print("[%s] %3s [%s] %3s [%s] ==> [%s] %3s [%s]" % \
+                               (start, nt1, mid, nt2, end, start, index[(nt1,nt2)], end))
+                        wfst[start][end] = index[(nt1,nt2)]
     return wfst 
