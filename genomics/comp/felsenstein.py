e9750898094da3919ab985536d678a349847dae6
Syed Hussain Ather <shussainather@gmail.com>
2019-05-23 04:25:51 -0400

    self

--- genomics/comp/felsenstein.py
+++ genomics/comp/felsenstein.py
@@ -1,3 +1,11 @@
 """
 Felstenstein's algorithm (felsenstein) for likelihood of phylogenetic trees.
 """
+
+class Node:
+    """
+    Initialize each node in the tree.
+    """
+    def __init__(self, position, leaf):
+        self.position = position
+        self.isLeaf = leaf 
