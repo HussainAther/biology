20d31e3f85b409a97dd2bc7862ef9aa3a4a1c490
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 02:03:06 -0400

    more costs

--- genomics/comp/carrillolipman.py
+++ genomics/comp/carrillolipman.py
@@ -6,4 +6,6 @@ S(A,A) = S(C,C) = S(G,G) = S(T,T) = 0,
 S(C,T) = S(T,C) = S(A,G) = S(G,A) = 1,
 S(A,T) = S(T,A) = S(A,C) = S(C,A)
        = S(G,C) = S(C,G) = S(G,T) = S(T,G) = 2
+
+with gap-open cost d = 3 and gap-extension cost e = 2.
 """
