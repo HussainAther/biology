2209709c4d4c9481a9941ce3e86ae96afce62e5e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 02:02:42 -0400

    init commit

---
+++ genomics/comp/carrillolipman.py
@@ -0,0 +1,9 @@
+"""
+Carrillo–Lipman multiple alignment algorithm (carrillo lipman).
+
+Substitution costs.
+S(A,A) = S(C,C) = S(G,G) = S(T,T) = 0, 
+S(C,T) = S(T,C) = S(A,G) = S(G,A) = 1,
+S(A,T) = S(T,A) = S(A,C) = S(C,A)
+       = S(G,C) = S(C,G) = S(G,T) = S(T,G) = 2
+"""
