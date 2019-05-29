67d3359501dbc469c51c2f1e296fa16d2c2b2038
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 00:35:00 -0400

    def

--- genomics/bio/localaffine.py
+++ genomics/bio/localaffine.py
@@ -1,3 +1,9 @@
 """
 Local alignment with affine gap penalty (LAFF).
 """
+
+def local_alignment_affine_gap_penalty(v, w, scoring_matrix, sigma, epsilon):
+    """
+    Return the score and local alignment substrings for strings v, w with the
+    given scoring matrix, gap opening penalty sigma, and gap extension penalty epsilon.
+    """
