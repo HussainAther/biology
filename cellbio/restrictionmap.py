4456f72a491f67fcc0037f66a949608910cfd930
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 01:04:29 -0400

    contain and return

--- cellbio/restrictionmap.py
+++ cellbio/restrictionmap.py
@@ -15,3 +15,14 @@ def partial_digest(distances):
     # Create lambda functions for multiset operations.
     new_dist = lambda y, S: Counter(abs(y-s) for s in S)
     containment = lambda a, b: all(a[x] <= b[x] for x in a)
+
+    # Create the multiset which generates distances.
+    while len(distances) > 0:
+        y = max(distances)
+        if containment(new_dist(y, X), distances):
+            X |= {y}
+            distances -= new_dist(y, X)
+        else:
+            X |= {width - y}
+            distances -= new_dist(width - y, X)
+    return X
