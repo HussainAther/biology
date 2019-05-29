d9e56eeea360a7d9aae2681e524298ce04ddf9b1
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 00:50:20 -0400

    def

--- genomics/bio/semiglobalalignment.py
+++ genomics/bio/semiglobalalignment.py
@@ -1,3 +1,11 @@
 """
 Semi-global (semi global semiglobal) alignment smgb.
 """
+
+def smgb(v, w, sigma):
+    """
+    Returns the semiglobal alignment of v and w and the associated score.
+    """
+    # Initialize the matrices.
+    S = [[0 for j in xrange(len(w)+1)] for i in xrange(len(v)+1)]
+    backtrack = [[0 for j in xrange(len(w)+1)] for i in xrange(len(v)+1)]
