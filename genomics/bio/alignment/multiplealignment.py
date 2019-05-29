892baed3c0604fa3e39b4e2a229e1ed64bdc88af
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 00:58:47 -0400

    init

--- genomics/bio/alignment/multiplealignment.py
+++ genomics/bio/alignment/multiplealignment.py
@@ -1,3 +1,7 @@
+from itertools import product
+from operator import add, mul
+from scipy.misc import comb
+
 """
 Multiple alignment.
 """
@@ -6,7 +10,10 @@ def multiple_alignment(word_list):
     """
     Return the multiple alignment of a given list of words.
     """
-    from itertools import product
-    from operator import add, mul
-    from scipy.misc import comb
+    word_list = ["$" + word for word in word_list]
+
+    # Initialize scoring and backtrack dictionaries, along with the indices and base score.
+    S, backtrack = {}, {}
+    perm_list = list(product([0, -1], repeat=len(word_list)))[1:]
+    base_score = -1*comb(len(word_list), 2, exact=True)
 
