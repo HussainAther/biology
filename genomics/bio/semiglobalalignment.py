63e69252ef7ba751377d2d96284f2c6ced0f64ad
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 00:51:02 -0400

    get pos

--- genomics/bio/semiglobalalignment.py
+++ genomics/bio/semiglobalalignment.py
@@ -16,3 +16,15 @@ def smgb(v, w, sigma):
             scores = [S[i-1][j] - sigma, S[i][j-1] - sigma, S[i-1][j-1] + [-1, 1][v[i-1] == w[j-1]]]
             S[i][j] = max(scores)
             backtrack[i][j] = scores.index(S[i][j])
+
+    # Get the position of the highest scoring cell in the last row or last column.
+    last_row_index = max(xrange(len(w)+1), key=lambda x: S[len(v)][x])
+    last_column_index = max(xrange(len(v)+1), key=lambda x: S[x][len(w)])
+    if S[len(v)][last_row_index] >= S[last_column_index][len(w)]:
+        i = len(v)
+        j = last_row_index
+    else:
+        i = last_column_index
+        j = len(w)
+    max_score = S[i][j]
+
