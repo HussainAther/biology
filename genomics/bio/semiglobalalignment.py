af38c84c94d4255cd0d96469b09274905b2fe39c
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 00:50:51 -0400

    score and backtrack

--- genomics/bio/semiglobalalignment.py
+++ genomics/bio/semiglobalalignment.py
@@ -9,3 +9,10 @@ def smgb(v, w, sigma):
     # Initialize the matrices.
     S = [[0 for j in xrange(len(w)+1)] for i in xrange(len(v)+1)]
     backtrack = [[0 for j in xrange(len(w)+1)] for i in xrange(len(v)+1)]
+
+    # Fill in the Score and Backtrack matrices.
+    for i in xrange(1, len(v)+1):
+        for j in xrange(1, len(w)+1):
+            scores = [S[i-1][j] - sigma, S[i][j-1] - sigma, S[i-1][j-1] + [-1, 1][v[i-1] == w[j-1]]]
+            S[i][j] = max(scores)
+            backtrack[i][j] = scores.index(S[i][j])
