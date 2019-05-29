790b7a84f62d3e8678fa4732c63f3e477cdd634a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 00:57:51 -0400

    sigma comment

--- genomics/bio/alignment/semiglobalalignment.py
+++ genomics/bio/alignment/semiglobalalignment.py
@@ -4,7 +4,8 @@ Semi-global (semi global semiglobal) alignment smgb.
 
 def smgb(v, w, sigma):
     """
-    Returns the semiglobal alignment of v and w and the associated score.
+    Return the semiglobal alignment of v and w and the associated score
+    within range sigma.
     """
     # Initialize the matrices.
     S = [[0 for j in xrange(len(w)+1)] for i in xrange(len(v)+1)]
