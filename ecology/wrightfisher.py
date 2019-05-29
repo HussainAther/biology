0ed2f4eda4ade46973e635efe086c3b946938da0
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 00:44:35 -0400

    bg

--- ecology/wrightfisher.py
+++ ecology/wrightfisher.py
@@ -1,3 +1,8 @@
+from scipy.misc import comb
+
 """
-Wright-Fisher model of genetic drift. (wright fisher)
+Wright-Fisher model of genetic drift. (wright fisher).
+First we determine the probabiliy of a given of recessive allels in the 
+first generation. We use a binomial random variable with the given parameters.
+We omit the 0th term throughout the problem, as it has no contribution to the desired probability.
 """
