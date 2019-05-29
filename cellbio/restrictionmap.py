fcbf61332ba8e6dc4b913745478f31e7a1624423
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 01:03:56 -0400

    def

--- cellbio/restrictionmap.py
+++ cellbio/restrictionmap.py
@@ -3,3 +3,15 @@ from collections import Counter
 """
 Create a restriction map, locations where restriction sites are in DNA.
 """
+
+def partial_digest(distances):
+    """
+    Return a set whose positive pairwise differences generate distances.
+    """
+    # Initialize variables.
+    X = {0}
+    width = max(distances)
+
+    # Create lambda functions for multiset operations.
+    new_dist = lambda y, S: Counter(abs(y-s) for s in S)
+    containment = lambda a, b: all(a[x] <= b[x] for x in a)
