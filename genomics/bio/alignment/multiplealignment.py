a93d9a8ca1a16135b691b5913e4018e3d1de016d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-29 00:57:57 -0400

    def

--- genomics/bio/alignment/multiplealignment.py
+++ genomics/bio/alignment/multiplealignment.py
@@ -1,3 +1,12 @@
 """
 Multiple alignment.
 """
+
+def multiple_alignment(word_list):
+    """
+    Return the multiple alignment of a given list of words.
+    """
+    from itertools import product
+    from operator import add, mul
+    from scipy.misc import comb
+
