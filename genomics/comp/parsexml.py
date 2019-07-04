b9f86ec93e39560e948dc8ff0ef3d93f5b2348e1
Syed Hussain Ather <shussainather@gmail.com>
2019-07-04 19:48:35 -0400

    class

--- genomics/comp/parsexml.py
+++ genomics/comp/parsexml.py
@@ -3,3 +3,12 @@ import xml.parsers.expat
 """
 Get content of several XML tags.
 """
+
+class GenomeBinomialParser:
+    ChunkSize = 4000 # number of bytes to read at a time
+    def __init__(self):
+        self.parser = xml.parsers.expat.ParserCreate()
+        self.parser.pbuffer_text = True
+        self.parser.pStartElementHandler = self.start_element
+        self.parser.pEndElementHandler = self.end_element
+        self.parser.pCharacterDataHandler = self.char_text
