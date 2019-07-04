9ec9ab351fbb8420a75ebd5c7a1bbe8639f76c02
Syed Hussain Ather <shussainather@gmail.com>
2019-07-04 17:58:00 -0400

    read it

--- genomics/comp/readenzyme.py
+++ genomics/comp/readenzyme.py
@@ -1,3 +1,11 @@
 """
 Read enzyme data.
 """
+
+def read_enzymes_from_file(filename):
+    """
+    Read enzymes.
+    """
+    with open(filename) as file:
+        skip_intro(file)
+        return (get_enzymes(file), get_references(file))
