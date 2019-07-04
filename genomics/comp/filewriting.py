db4f1f085cbfc88c4947a13d115c1d5da9a840ad
Syed Hussain Ather <shussainather@gmail.com>
2019-07-04 16:04:30 -0400

    init var

--- genomics/comp/filewriting.py
+++ genomics/comp/filewriting.py
@@ -3,3 +3,18 @@ For writing to files.
 
 Draw a sequence trace using SVG polylines.
 """
+
+# Initialize.
+colors = ("black", "green", "red", "blue") 
+axiscolor = "#333"
+axisthickness = 2
+tickcolor = "#666"
+tickspacing = 100 
+tickheight = 18
+tickthickness = 2
+maxval = 1600
+leftmargin = 10
+topmargin = 20
+yzero = 100
+hscale = 5/2 
+vscale = 14
