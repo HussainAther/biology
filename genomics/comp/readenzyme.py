5f99675fb554126fe60cac1bff07df88e9e1a919
Syed Hussain Ather <shussainather@gmail.com>
2019-07-04 18:00:02 -0400

    Skip intro

--- genomics/comp/readenzyme.py
+++ genomics/comp/readenzyme.py
@@ -9,3 +9,15 @@ def read_enzymes_from_file(filename):
     with open(filename) as file:
         skip_intro(file)
         return (get_enzymes(file), get_references(file))
+
+def skip_intro(file):
+    """
+    Skip through the documentation that appears at the beginning
+    of file, leaving it positioned at the first line of the first enzyme.
+    """
+    line = ""
+    while not line.startswith('<REFERENCES>'):
+        line = file.readline()
+    while len(line) > 1: # always 1 for '\n'
+        line = file.readline()
+    return line
