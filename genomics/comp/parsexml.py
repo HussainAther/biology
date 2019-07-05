10509122cba90e1360f2da6344af0b27566ec92d
Syed Hussain Ather <shussainather@gmail.com>
2019-07-04 20:04:44 -0400

    defs

--- genomics/comp/parsexml.py
+++ genomics/comp/parsexml.py
@@ -12,3 +12,29 @@ class GenomeBinomialParser:
         self.parser.pStartElementHandler = self.start_element
         self.parser.pEndElementHandler = self.end_element
         self.parser.pCharacterDataHandler = self.char_text
+
+     def start_element(self, name, attrs):
+         """
+         Record name of start tag just encountered.
+         """
+         self.current_tag = name # ignore attrs
+    
+     def end_element(self, name):
+         """
+         Stop parsing when species name has been encountered.
+         """
+         if name == 'BinomialOrgName_species':
+             raise StopIteration
+    
+     def char_text(self, text):
+         """
+         If current tag is for genus or species, record its content.
+         """
+         if not text.isspace():
+             # checking for whitespace because this function gets called
+             # for both tag content and whitespace between the end of
+             # one tag and the beginning of the next
+             if self.current_tag == 'BinomialOrgName_genus':
+                 self.genus = text.strip()
+             elif self.current_tag == 'BinomialOrgName_species':
+                 self.species = text.strip()
