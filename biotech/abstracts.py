a5540a0c00202b348e7c4ff3d99106d8dc280ec3
Syed Hussain Ather <shussainather@gmail.com>
2019-07-17 00:44:50 -0400

    email steps

--- biotech/abstracts.py
+++ biotech/abstracts.py
@@ -5,3 +5,16 @@ from tqdm import tqdm
 """
 Biopython work for fetching abstracts.
 """
+
+# Change this email to your email address
+Entrez.email = "shussainather@gmail.com"
+
+keyword = "optical trap"
+
+result = Entrez.read(Entrez.esearch(db="pubmed", retmax=10, term=keyword))
+print(
+    "Total number of publications that contain the term {}: {}".format(
+        keyword, result["Count"]
+    )
+)
+
