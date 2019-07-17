e27d2a9228697381a557ba25f41b80109e69fa8f
Syed Hussain Ather <shussainather@gmail.com>
2019-07-17 00:46:44 -0400

    top authors

--- biotech/abstracts.py
+++ biotech/abstracts.py
@@ -40,3 +40,21 @@ for batch in tqdm(batches):
     records = Medline.parse(h)
     record_list.extend(list(records))
 print("Complete.")
+
+sns.set_style("white")
+
+publication_data = pd.DataFrame(record_list)
+publication_data.dropna(subset=['EDAT'], inplace=True)
+publication_data["Year"] = (
+    publication_data["EDAT"].astype(str).str[0:4].astype(int)
+)
+
+plt.figure(figsize=(10, 10), dpi=600)
+
+# Top 10 authors
+plt.subplot(2, 2, 1)
+authors_flat = [
+    author
+    for authors in list(publication_data["FAU"].dropna())
+    for author in authors
+]
