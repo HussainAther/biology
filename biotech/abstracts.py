709b3830c27a35313a3cde6e958d7629697080dc
Syed Hussain Ather <shussainather@gmail.com>
2019-07-17 00:52:27 -0400

    network

--- biotech/abstracts.py
+++ biotech/abstracts.py
@@ -147,3 +147,32 @@ c = CircosPlot(
 )
 c.draw()
 plt.show()
+
+# Network analysis
+deg = nx.degree_centrality(G_50)
+bet = nx.betweenness_centrality(G_50)
+
+top_df = pd.DataFrame.from_dict(
+    [deg, bet, dict(Counter(authors_flat).most_common(50))]
+).T
+top_df.columns = [
+    "Degree Centrality",
+    "Betweenness Centrality",
+    "Publications",
+]
+
+for col in top_df.columns:
+    top_df[col] = top_df[col] / max(top_df[col])
+
+top_df = top_df.sort_values("Publications", ascending=False)[:10]
+top_df = pd.DataFrame(top_df.stack())
+top_df = top_df.reset_index()
+top_df.columns = ["Name", "Parameter", "Value"]
+
+fig, ax = plt.subplots(figsize=(10, 8), dpi=600)
+
+sns.barplot(x="Value", y="Name", data=top_df, hue="Parameter", palette="Blues")
+
+plt.title("Top 10 Authors, normalized parameters")
+plt.show()
+
