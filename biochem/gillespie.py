86f509fd5dbe05a925f8bb2705e549d39a5dca7c
Syed Hussain Ather <shussainather@gmail.com>
2019-08-28 16:27:25 -0400

    seaborn settings

--- biochem/gillespie.py
+++ biochem/gillespie.py
@@ -10,3 +10,10 @@ Gillespie algorithm simulation on a simple production of a protein.
 """
 
 %load_ext line_profiler
+
+rc = {"lines.linewidth": 2, 
+      "axes.labelsize": 18, 
+      "axes.titlesize": 18, 
+      "axes.facecolor": "DFDFE5"}
+sns.set_context("notebook", rc=rc)
+sns.set_style("darkgrid", rc=rc)
