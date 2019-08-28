9140d11307ff4d6f52cb46767a56216f157daf21
Syed Hussain Ather <shussainather@gmail.com>
2019-08-28 16:20:15 -0400

    import

--- biochem/gillespie.py
+++ biochem/gillespie.py
@@ -1,3 +1,10 @@
+import multiprocessing
+import numpy as np
+import scipy.stats as st
+import numba
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 """
 Gillespie algorithm simulation on a simple production of a protein.
 """
