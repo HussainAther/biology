2512ccce504e0038155534ea0d5748029c31d427
Syed Hussain Ather <shussainather@gmail.com>
2019-09-19 21:45:38 -0700

    count

--- biotech/history.py
+++ biotech/history.py
@@ -4,3 +4,5 @@ search_results = Entrez.read(Entrez.esearch(db="pubmed",
                                             term="Opuntia[ORGN]",
                                             reldate=365, datetype="pdat",
                                             usehistory="y"))
+count = int(search_results["Count"])
+print("Found %i results" % count)
