91908cfa0c3b74b7fc885b40f287e408e92e502c
Syed Hussain Ather <shussainather@gmail.com>
2019-09-19 21:44:54 -0700

    entrez

--- biotech/history.py
+++ biotech/history.py
@@ -1,2 +1,6 @@
 from Bio import Entrez
-
+Entrez.email = "history.user@example.com"
+search_results = Entrez.read(Entrez.esearch(db="pubmed",
+                                            term="Opuntia[ORGN]",
+                                            reldate=365, datetype="pdat",
+                                            usehistory="y"))
