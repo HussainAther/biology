03fe1190b4d39c7201bdd0b959aecd35774dde67
Syed Hussain Ather <shussainather@gmail.com>
2019-09-19 21:46:05 -0700

    loop through

--- biotech/history.py
+++ biotech/history.py
@@ -6,3 +6,13 @@ search_results = Entrez.read(Entrez.esearch(db="pubmed",
                                             usehistory="y"))
 count = int(search_results["Count"])
 print("Found %i results" % count)
+batch_size = 10
+out_handle = open("recent_orchid_papers.txt", "w")
+for start in range(0,count,batch_size):
+    end = min(count, start+batch_size)
+    print("Going to download record %i to %i" % (start+1, end))
+    fetch_handle = Entrez.efetch(db="pubmed",rettype="medline",
+                                 retmode="text",retstart=start,
+                                 retmax=batch_size,
+                                 webenv=search_results["WebEnv"],
+                                 query_key=search_results["QueryKey"])
