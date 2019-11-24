8800c931fadf057369df601a814e62db1ebab4be
Syed Hussain Ather <shussainather@gmail.com>
2019-11-23 23:38:31 -0800

    other funcs

--- ecology/hawkdove.py
+++ ecology/hawkdove.py
@@ -123,5 +123,154 @@ def init():
 		a2 = Agent()
 		a2.agent_type = hawk
 		agents.append(a2)
+		
+def getAvgFromList(list):
+	return float( sum(list) / len(list) )
+
+
+def getTimeFormatted(seconds):
+	m, s = divmod(seconds, 60)
+	return "%02d:%02d" % (m, s)	
+
+
+def getFood():
+	return randint(minfoodround, maxfoodround)
+
+
+def getPercByType(agent_type):
+	perc = float(getAgentCountByType(agent_type)) / float(len(agents))
+	return '{percent:.2%}'.format(percent=perc)
+
+
+def getAliveAgentsCount():
+	return getAgentCountByStatus(statusactive) + getAgentCountByStatus(statusasleep)
+
+
+def getRandomAgents():
+	nemesis = None
+	active_agents = list(generateAgentsByStatus(statusactive))
+	if len(active_agents) < 2:
+		return None, None
+	max_index = len(active_agents) - 1
+	agent = active_agents[ randint(0, max_index) ]
+	while nemesis is None:
+		n = active_agents[ randint(0, max_index) ]
+		if n is not agent:
+			nemesis = n
+
+	return agent, nemesis
+
+
+def awakenAgents():
+	for agent in agents:
+		agent.status = statusactive
+
+
+def generateAgentsByType(agent_type):
+	for agent in agents:
+		if agent.agent_type == agent_type:
+			yield agent
+
+
+def generateAgentsByStatus(status):
+	for agent in agents:
+		if agent.status == status:
+			yield agent
+
+
+def getEnergyFromFood(food):
+	return food # 1 to 1
+
+
+def getAgentCountByStatus(status):
+	count = len( list(generateAgentsByStatus(status)) )
+	return count
+
+
+def getAgentCountByType(agent_type):
+	return len( list(generateAgentsByType(agent_type)) )
+
+
+def compete(agent, nemesis, food):
+	winner = choice([agent, nemesis])
+	loser = agent if (winner is nemesis) else nemesis
+
+	if agent.agent_type == hawk and nemesis.agent_type == hawk:
+		# Random winner chosen, loser gets injured, winner gets food
+		winner.energy += getEnergyFromFood(food)
+		loser.energy  -= energylossfight
+
+	if agent.agent_type == hawk and nemesis.agent_type == dove:
+		agent.energy += getEnergyFromFood(food)
+		nemesis.energy -= energybluffcost
+
+	if agent.agent_type == dove and nemesis.agent_type == hawk:
+		nemesis.energy += getEnergyFromFood(food)
+		agent.energy -= energybluffcost
+
+	if agent.agent_type == dove and nemesis.agent_type == dove:
+		winner.energy += getEnergyFromFood(food)
+		loser.energy  -= energybluffcost
+
+	nemesis.status = agent.status = statusasleep
+
+
+def getNewAgent(agent_type, starting_energy=startenergy, status=statusasleep):
+	agent = Agent()
+	agent.agent_type = agent_type
+	agent.status = status
+	agent.energy = starting_energy
+	return agent
+
+
+def breed():
+	"""
+	If agent can breed, it halves its energy and produces 
+	two babies with starting energy (parent energy / 2)
+	"""
+	hawk_babies = 0
+	dove_babies = 0
+	for agent in agents:
+		if agent.energy > repenergy:
+			baby_agent_a = getNewAgent(agent.agent_type, (agent.energy/2))
+			baby_agent_b = getNewAgent(agent.agent_type, (agent.energy/2))
+
+			agents.append(baby_agent_b)
+			agents.append(baby_agent_a)
+
+			agent.energy /= 2
+
+			if agent.agent_type == dove: dove_babies += 2
+			if agent.agent_type == hawk: hawk_babies += 2
+
+
+	return hawk_babies, dove_babies
+
+
+def cull():
+
+	dead_hawks = 0
+	dead_doves = 0
+	for index, agent in enumerate(agents):
+		if agent.energy < livingenergy:
+			if agent.agent_type == dove: dead_doves += 1
+			if agent.agent_type == hawk: dead_hawks += 1
+			del agents[index]
+
+
+	return dead_hawks, dead_doves
+
+
+main()
+
+try:
+	from pylab import *
+except ImportError:
+	exit()
+else:
+	plot(graph_dove_points)
+	plot(graph_hawk_points)
+	show()
+
 
 
