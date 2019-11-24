2b8694b32aa60ddcb71efa44acda4255c373f7c0
Syed Hussain Ather <shussainather@gmail.com>
2019-11-23 23:37:05 -0800

    init

--- ecology/hawkdove.py
+++ ecology/hawkdove.py
@@ -5,3 +5,28 @@ from random import choice, randint
 """
 Evolutionary game theory (EGT) Hawk-dove (hawk dove hawkdove) game.
 """
+
+startdoves = 100
+starthawks = 100
+startpop = starthawks + startdoves
+
+rounds = 20
+startenergy = 100;
+
+minfoodround = 20
+maxfoodround = 70
+
+repenergy = 250
+energylossround = 2
+energybluffcost = 10
+energylossfight = 200
+livingenergy = 20
+
+statusactive = "active"
+statusasleep = "asleep"
+
+hawk = "hawk"
+dove = "dove"
+
+agents = []
+
