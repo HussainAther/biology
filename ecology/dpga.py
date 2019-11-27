56463b624db683f6c5cd9544734031e8e6a3ef62
Syed Hussain Ather <shussainather@gmail.com>
2019-11-26 17:49:41 -0800

    initialize

--- ecology/dpga.py
+++ ecology/dpga.py
@@ -2,8 +2,15 @@
 Dual-Population Genetic Algorithm (dgpa) for Adaptive Diversity Control
 """
 
+m = 10 # main population initial amount
+n = 3 # reserve population initial amount
+
+M = [m] # list of main population amounts at each time interval
+R = [n] # same for reserve pop
+
 def dpga():
     """
     Maintain population diversity with genetic algorithms. Dual-population
     genetic algorithm does that using an extra population for more diversity.
     """
+    
