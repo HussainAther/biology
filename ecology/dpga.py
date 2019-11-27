9bc84e1b50912656182b88d98209d96d9182b6e6
Syed Hussain Ather <shussainather@gmail.com>
2019-11-26 17:43:19 -0800

    background

--- ecology/dpga.py
+++ ecology/dpga.py
@@ -1,3 +1,9 @@
 """
 Dual-Population Genetic Algorithm (dgpa) for Adaptive Diversity Control
 """
+
+def dpga():
+    """
+    Maintain population diversity with genetic algorithms. Dual-population
+    genetic algorithm does that using an extra population for more diversity.
+    """
