b9a8b01da6227d603b6f9ca0436217e1609ccb2c
Syed Hussain Ather <shussainather@gmail.com>
2019-11-26 18:12:05 -0800

    Fit fit fit

--- ecology/dpga.py
+++ ecology/dpga.py
@@ -8,9 +8,31 @@ n = 3 # reserve population initial amount
 M = [m] # list of main population amounts at each time interval
 R = [n] # same for reserve pop
 
-def dpga():
+def rosenbrock(X):
+    """
+    This R^2 -> R^1 function should be compatible with algopy.
+    http://en.wikipedia.org/wiki/Rosenbrock_function
+    A generalized implementation is available
+    as the scipy.optimize.rosen function.
+    """
+    x = X[0]
+    y = X[1]
+    a = 1. - x
+    b = y - x*x
+    return a*a + b*b*100.
+
+def fm(M):
+    """
+    Use a fitness function to evaluate the main population.
+    """
+    1.00001*max(M) - rosenbrock(M) 
+
+def dpga(tmax):
     """
     Maintain population diversity with genetic algorithms. Dual-population
     genetic algorithm does that using an extra population for more diversity.
+    Loop until tmax.
     """
-    
+    for i in range(tmax):
+        
+     
