92957399d4df9bee7fff942a8c87a7a0ab780512
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 11:21:07 -0800

    group data

--- genomics/comp/factor.R
+++ genomics/comp/factor.R
@@ -1,7 +1,35 @@
 library(knitr)
+library(rafalib)
+library(RColorBrewer)
+library(genefilter)
+library(GSE5859Subset)
 opts_chunk$set(fig.path=paste0("figure/", sub("(.*).Rmd","\\1",basename(knitr:::knit_concord$get('infile'))), "-"))
 
 # Model batch effects with factor analysis
 
-library(GSE5859Subset)
 data(GSE5859Subset)
+
+# group the data by features 
+sex <- sampleInfo$group
+batch <- factor(format(sampleInfo$date,"%m"))
+chr <- geneAnnotation$CHR
+
+tt<-rowttests(geneExpression,batch)
+
+ind1 <- which(chr=="chrY") # real differences
+ind2 <- setdiff(c(order(tt$dm)[1:25],order(-tt$dm)[1:25]),ind1)
+
+set.seed(1)
+ind0 <- setdiff(sample(seq(along=tt$dm),50),c(ind2,ind1))
+geneindex<-c(ind2,ind0,ind1)
+mat<-geneExpression[geneindex,]
+mat <- mat -rowMeans(mat)
+icolors <- colorRampPalette(rev(brewer.pal(11,"RdYlBu")))(100)
+
+mypar(1,2)
+image(t(mat),xaxt="n",yaxt="n",col=icolors)
+y <- geneExpression - rowMeans(geneExpression)
+image(1:ncol(y),1:ncol(y),cor(y),col=icolors,zlim=c(-1,1),
+       xaxt="n",xlab="",yaxt="n",ylab="")
+axis(2,1:ncol(y),sex,las=2)
+axis(1,1:ncol(y),sex,las=2)
