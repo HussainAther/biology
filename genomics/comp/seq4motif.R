772f606e544586879bb09502f1f9ec2f82fd9770
Syed Hussain Ather <shussainather@gmail.com>
2020-01-12 10:45:11 -0800

    import library

--- genomics/comp/seq4motif.R
+++ genomics/comp/seq4motif.R
@@ -1,4 +1,10 @@
 library(knitr)
+library(GenomicFeatures)
+library(GenomicRanges)
+library(IRanges)
+library(devtools)
+library(ERBS)
+library(Homo.sapiens)
 opts_chunk$set(fig.path=paste0("figure/", sub("(.*).Rmd","\\1",basename(knitr:::knit_concord$get('infile'))), "-"))
 
 # Genomic sequence utility for motif tracking
