2c53d64029a76e4734aca8f16ada23d3d17a54db
Syed Hussain Ather <shussainather@gmail.com>
2023-04-16 17:34:56 -0400

    function and coment

--- biotech/seqio.py
+++ biotech/seqio.py
@@ -3,4 +3,10 @@ from Bio import SeqIO
 INPUT_FILE = '../../samples/fasta22.fas'
 OUTPUT_FILE = 'fasta22_out.fas'
 
-
+def retseq(seq_fh):
+    """
+    Parse a fasta file and store non empty records
+    into the fullseqs list.
+    :seq_fh: File handle of the input seqeuce
+    :return: A list with non empty sequences
+    """
